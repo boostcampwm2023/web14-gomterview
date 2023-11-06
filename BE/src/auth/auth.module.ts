51108d2a9f05a69bdbc48de060a711a8a59989b0
JangAJang <janghee5395@gmail.com>
2023-11-06 16:36:00 +0900

    feat: passport strategy를 통해 OAuth에서 회원 정보를 불러오는 플로우 구현
    Co-authored-by: quiet-honey <skap0329@naver.com>

--- BE/src/auth/auth.module.ts
+++ BE/src/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { AuthController } from './controller/auth.controller';
 import { MemberRepository } from 'src/member/repository/member.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Member } from 'src/member/entity/member';
+import { GoogleStrategy } from './strategy/google.strategy';
 @Module({
   imports: [TypeOrmModule.forFeature([Member])],
-  providers: [AuthService, MemberRepository],
+  providers: [AuthService, MemberRepository, GoogleStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
