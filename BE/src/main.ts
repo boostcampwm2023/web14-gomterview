d2bcf314490f0a595044aa21155f0986c0c22970
JangAJang <janghee5395@gmail.com>
2023-11-06 20:11:24 +0900

    chore: swagger가 메인 프로젝트에 실행되게 코드 추가

--- BE/src/main.ts
+++ BE/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { setupSwagger } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  setupSwagger(app);
   await app.listen(8080);
 }
 bootstrap();
