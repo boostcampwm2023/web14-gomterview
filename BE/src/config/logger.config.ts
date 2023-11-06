4fb0bdac8c76cc27f8942d32183211505c0ad5d1
JangAJang <janghee5395@gmail.com>
2023-11-06 12:21:58 +0900

    feat: winsto을 이용한 로깅 모듈 구현
    Co-authored-by: quiet-honey <skap0329@naver.com>

---
+++ BE/src/config/logger.config.ts
@@ -0,0 +1,69 @@
+import { LoggerService as LS } from '@nestjs/common';
+import * as winston from 'winston';
+import { utilities as nestWinstonModuleUtilities } from 'nest-winston';
+
+const { errors, combine, timestamp, printf } = winston.format;
+
+export class LoggerService implements LS {
+  private logger: winston.Logger;
+
+  constructor(service: string) {
+    this.logger = winston.createLogger({
+      transports: [
+        new winston.transports.File({
+          level: 'error',
+          filename: `error-${new Date().toLocaleString()}.log`,
+          dirname: 'logs',
+          maxsize: 5000000,
+          format: combine(
+            errors({ stack: true }),
+            timestamp({ format: 'isoDateTime' }),
+            printf((info) => {
+              return `${info.message}`;
+            }),
+          ),
+        }),
+        new winston.transports.Console({
+          level: 'debug',
+          format: combine(
+            timestamp({ format: 'isoDateTime' }),
+            nestWinstonModuleUtilities.format.nestLike(service, {
+              prettyPrint: true,
+            }),
+          ),
+        }),
+
+        new winston.transports.File({
+          filename: `application-${new Date().toLocaleString()}.log`,
+          dirname: 'logs',
+          maxsize: 5000000,
+          format: combine(
+            timestamp({ format: 'isoDateTime' }),
+            printf((info) => {
+              return `${info.message}`;
+            }),
+          ),
+        }),
+      ],
+    });
+  }
+
+  log(message: string) {
+    this.logger.log({ level: 'info', message });
+  }
+  info(message: string) {
+    this.logger.info(message);
+  }
+  error(message: string, trace: string) {
+    this.logger.error(message, trace);
+  }
+  warn(message: string) {
+    this.logger.warning(message);
+  }
+  debug(message: string) {
+    this.logger.debug(message);
+  }
+  verbose(message: string) {
+    this.logger.verbose(message);
+  }
+}
