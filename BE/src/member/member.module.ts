d4403406da37294a32e6affebaec634aba3dbf99
JangAJang <janghee5395@gmail.com>
2023-11-06 14:54:59 +0900

    refactor: MemberRepository를 다른 모듈에서 사용하게 exports추가

--- BE/src/member/member.module.ts
+++ BE/src/member/member.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { MemberService } from './member.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Member } from './entity/member';
+import { MemberRepository } from './repository/member.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Member])],
-  providers: [MemberService],
+  providers: [MemberRepository, MemberService],
+  exports: [MemberRepository],
 })
 export class MemberModule {}
