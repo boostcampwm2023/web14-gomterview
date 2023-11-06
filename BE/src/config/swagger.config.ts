8b7ffd28b010b453eaf306882d63e78390c999f4
JangAJang <janghee5395@gmail.com>
2023-11-06 12:24:55 +0900

    feat: swagger config 모듈 구현
    Co-authored-by: quiet-honey <skap0329@naver.com>

---
+++ BE/src/config/swagger.config.ts
@@ -0,0 +1,13 @@
+import { INestApplication } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication): void {
+  const options = new DocumentBuilder()
+    .setTitle('NDD')
+    .setDescription("봉준호, BTS, 손흥민, NDD Let's go")
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+}
