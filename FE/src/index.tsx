68c2efd41c2bae9bf8a42e5c3da31ad605811363
adultlee <seoungin1228@gmail.com>
2023-11-06 16:42:54 +0900

    feat: 상대 경로 설정

--- FE/src/index.tsx
+++ FE/src/index.tsx
@@ -1,10 +1,10 @@
-import App from './App';
+import App from '@/App';
 import ReactDOM from 'react-dom/client';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
-  root.render(App());
+  root.render(<App />);
 } else {
   console.error('Root element not found');
 }
