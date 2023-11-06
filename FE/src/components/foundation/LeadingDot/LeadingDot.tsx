67b931ff5c827be353df0234179e10a130f003e5
milk717 <robolindasoo@gmail.com>
2023-11-07 08:19:27 +0900

    feat: 왼쪽에 가상요소로 점을 추가해주는LeadingDot 컴포넌트 추가
    - 녹화중, 연결완료, 연결안됨, 문제 선택기 등에 사용하기 위함.

---
+++ FE/src/components/foundation/LeadingDot/LeadingDot.tsx
@@ -0,0 +1,41 @@
+import { FC, ReactNode } from 'react';
+import { css } from '@emotion/react';
+
+type LeadingDotProps = {
+  children: ReactNode;
+  color?: string;
+  gap?: string;
+  size?: string;
+};
+
+export const LeadingDot: FC<LeadingDotProps> = ({
+  children,
+  color = '#76d773',
+  gap = '0.5rem',
+  size = '0.5rem',
+}) => {
+  return (
+    <div
+      css={css`
+        display: flex;
+        position: relative;
+        padding-left: calc(${size} + ${gap});
+        overflow: hidden;
+
+        &:before {
+          content: '';
+          position: absolute;
+          left: 0;
+          top: 50%;
+          transform: translateY(-50%);
+          width: ${size};
+          height: ${size};
+          background-color: ${color};
+          border-radius: 50%;
+        }
+      `}
+    >
+      {children}
+    </div>
+  );
+};
