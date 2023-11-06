c6f9f26a3b35c8835359887fca67a6fc6d952e06
JangAJang <janghee5395@gmail.com>
2023-11-06 17:00:11 +0900

    feat: 회원가입을 위해서 DB 조회후 검증하는 로직 구현
    Co-authored-by: quiet-honey <skap0329@naver.com>

--- BE/src/auth/service/auth.service.ts
+++ BE/src/auth/service/auth.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { MemberRepository } from 'src/member/repository/member.repository';
 import { OAuthRequest } from '../dto/auth.interface';
+import { Member } from 'src/member/entity/member';
 
 @Injectable()
 export class AuthService {
   constructor(private memberRepository: MemberRepository) {}
 
   async login(oauthRequest: OAuthRequest) {
-    // 1. DB에서 조회
-    // 2. 없다면 회원 저장
+    const member = await this.memberRepository.findByEmail(oauthRequest.email);
+
+    if (isMemberNotFound(member)) {
+      // 회원가입 진행
+    }
     // 3. finally 토큰 반환(일단 json반환)
   }
 }
+
+const isMemberNotFound = (member: Member | undefined) => member === undefined;
