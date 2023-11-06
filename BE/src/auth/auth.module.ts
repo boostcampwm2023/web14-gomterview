0cd338fcc47f08b92f9b9927389087aefa7dc93d
JangAJang <janghee5395@gmail.com>
2023-11-06 16:06:31 +0900

    feat: member모듈에서 리포지토리를 가져와 사용하는 Auth 모듈 구현
    Co-authored-by: quiet-honey <skap0329@naver.com>

---
+++ BE/src/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './service/auth.service';
+import { AuthController } from './controller/auth.controller';
+import { MemberRepository } from 'src/member/repository/member.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Member } from 'src/member/entity/member';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Member])],
+  providers: [AuthService, MemberRepository],
+  controllers: [AuthController],
+})
+export class AuthModule {}
