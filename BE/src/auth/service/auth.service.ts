fc10b8a4b1b64ab79f65ecd7dd1019eaa292cc71
JangAJang <janghee5395@gmail.com>
2023-11-06 16:58:25 +0900

    feat: 로그인 로직 함수 추가 && 내부에 워크플로우 정리
    Co-authored-by: quiet-honey <skap0329@naver.com>

--- BE/src/auth/service/auth.service.ts
+++ BE/src/auth/service/auth.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { MemberRepository } from 'src/member/repository/member.repository';
+import { OAuthRequest } from '../dto/auth.interface';
 
 @Injectable()
 export class AuthService {
   constructor(private memberRepository: MemberRepository) {}
+
+  async login(oauthRequest: OAuthRequest) {
+    // 1. DB에서 조회
+    // 2. 없다면 회원 저장
+    // 3. finally 토큰 반환(일단 json반환)
+  }
 }
