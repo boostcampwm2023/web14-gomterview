dfc1fd93990f88d1c450b103e15b0a33ceeb43ff
Sumin <57657868+milk717@users.noreply.github.com>
2023-11-06 22:25:34 +0900

    [NDD-58] 테마 시스템 정의 (3h/3h) (#9)
    * feat: emotion ThemeProvider 추가하고 color, typography 추가
    
    - colorChips에 있는 색상은 프로젝트에서 바로 사용하지 않습니다.
    - colorChips에 있는 색상을 활용해서 color 시스템을 구성합니다.
    
    * feat: emotion global 스타일에 resetCss 추가
    
    * feat: emotion global 스타일에 pretendard 웹폰트로 적용
    
    * rename: resetCss -> _global 로 이름 변경
    
    * chore: 경로 별칭에 styles 디렉터리 추가

---
+++ FE/src/styles/_typography.ts
@@ -0,0 +1,64 @@
+export const typography = {
+  /**
+   * 페이지의 제목에 사용됩니다.
+   */
+  title1: {
+    fontFamily: 'Pretendard',
+    fontStyle: 'normal',
+    fontWeight: 700,
+    fontSize: '2rem',
+  },
+
+  /**
+   * 탭이나 모달의 제목에 사용됩니다.
+   * 마이페이지의 사용자 닉네임에도 사용됩니다
+   * 큰 버튼의 텍스트로도 사용됩니다.
+   */
+  title2: {
+    fontFamily: 'Pretendard',
+    fontStyle: 'normal',
+    fontWeight: 700,
+    fontSize: '1.5rem',
+  },
+
+  /**
+   * 기본 글자에는 모두 이 스타일이 사용됩니다.
+   */
+  body1: {
+    fontFamily: 'Pretendard',
+    fontStyle: 'normal',
+    fontWeight: 400,
+    fontSize: '1rem',
+  },
+
+  /**
+   * 동영상 페이지의 대본 글자, 모달의 body 등에 사용되는 스타일입니다.
+   */
+  body2: {
+    fontFamily: 'Pretendard',
+    fontStyle: 'normal',
+    fontWeight: 400,
+    fontSize: '1.125rem',
+  },
+
+  /**
+   * 마이페이지의 이메일, 마이페이지의 동영상 타이틀에 사용됩니다.
+   */
+  body3: {
+    fontFamily: 'Pretendard',
+    fontStyle: 'normal',
+    fontWeight: 400,
+    fontSize: '0.875rem',
+  },
+
+  /**
+   * Progress Step Bar, 녹화중 표시 등
+   * 사이즈 12px에는 모두 이 스타일이 사용됩니다.
+   */
+  captionBold: {
+    fontFamily: 'Pretendard',
+    fontStyle: 'normal',
+    fontWeight: 600,
+    fontSize: '0.75rem',
+  },
+};
