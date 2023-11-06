95287339ff4c65a96f9b0def61b71c094949a10f
JangAJang <janghee5395@gmail.com>
2023-11-06 16:06:31 +0900

    feat: member모듈에서 리포지토리를 가져와 사용하는 Auth 모듈 구현
    Co-authored-by: quiet-honey <skap0329@naver.com>

--- BE/src/app.module.ts
+++ BE/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MYSQL_OPTION } from './config/typeorm.config';
 import { MemberModule } from './member/member.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(MYSQL_OPTION), MemberModule],
+  imports: [TypeOrmModule.forRoot(MYSQL_OPTION), MemberModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
