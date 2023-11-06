42e4f202a5169486693c871f1756c61b428dc046
JangAJang <janghee5395@gmail.com>
2023-11-06 16:56:33 +0900

    feat: OAuth에서 회원 존재 여부를 검증하기 위한 리포지토리 로직 구현
    Co-authored-by: quiet-honey <skap0329@naver.com>

--- BE/src/member/repository/member.repository.ts
+++ BE/src/member/repository/member.repository.ts
@@ -8,4 +8,16 @@ export class MemberRepository {
   constructor(
     @InjectRepository(Member) private memberRepository: Repository<Member>,
   ) {}
+
+  async save(member: Member) {
+    await this.memberRepository.save(member);
+  }
+
+  async findById(id: number) {
+    return await this.memberRepository.findOneBy({ id: id });
+  }
+
+  async findByEmail(email: string) {
+    return await this.memberRepository.findOneBy({ email: email });
+  }
 }
