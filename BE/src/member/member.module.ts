cca06263ce6aa5a701909a50eaf27dc67b25f461
quiet-honey <skap0329@naver.com>
2023-11-08 00:57:10 +0900

    feat: service 계층에서 사용자의 정보를 얻어내는 로직 구현
    1. tokenService의 getPayload를 사용하여 accessToken에 담긴 member의 Id를 얻어옴 (public 메서드로 변경)
    2. member의 Id를 사용해 repository 계층에서 사용자의 정보를 얻어옴

--- BE/src/member/member.module.ts
+++ BE/src/member/member.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Member } from './entity/member';
 import { MemberRepository } from './repository/member.repository';
 import { MemberController } from './controller/member.controller';
+import { TokenModule } from 'src/token/token.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Member])],
+  imports: [TypeOrmModule.forFeature([Member]), TokenModule],
   providers: [MemberRepository, MemberService],
   exports: [MemberRepository],
   controllers: [MemberController],
--- BE/src/member/service/member.service.ts
+++ BE/src/member/service/member.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { MemberRepository } from '../repository/member.repository';
+import { TokenService } from 'src/token/service/token.service';
 
 @Injectable()
 export class MemberService {
-  constructor(private memberRepository: MemberRepository) {}
+  constructor(
+    private tokenService: TokenService,
+    private memberRepository: MemberRepository,
+  ) {}
 
-  getMyInfo(accessToken: string) {
-    const user = this.memberRepository.findById(null); // tokenService에서 토큰 소유자의 id 알아낸 후 repository 단에 넘겨주기 구현 필요
+  async getMyInfo(accessToken: string) {
+    const memberId = (await this.tokenService.getPayload(accessToken)).id;
+    const user = await this.memberRepository.findById(memberId);
     return user; // user 정보를 응답 형식에 맞게 dto화 후 반환 필요
   }
 }
