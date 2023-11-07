9dd08c018e40e1ae17f4878ea28ac94ace85b2cb
YoonHaeMin <yunhatmi@gachon.ac.kr>
2023-11-07 15:58:47 +0900

    [NDD-75] foundation 컴포넌트 디자인 구현 (1h/1h) (#11)
    * feat: element의 기본 props를 지정해주는 유틸 타입 추가
    
    * design: Avatar 컴포넌트 생성
    
    - theme을 이용해서 색상 정의
    - 세부 디자인은 외부에서 주입할 수 있도록 구현
    
    * feat: InputArea 컴포넌트 생성
    
    - theme을 이용해서 색상 정의
    - 세부 디자인은 외부에서 주입할 수 있도록 구현
    
    * feat: color system추가
    
    - 버튼 hover시 색상 추가
    - 색상 값에 as const를 붙여 hove시 색상 값이 노출 되도록 설정
    
    * feat: button 컴포넌트 생성
    
    - theme을 이용해서 색상 정의
    - 세부 디자인은 외부에서 주입할 수 있도록 구현
    
    * feat: Box 컴포넌트 생성
    
    - theme을 이용해서 색상 정의
    - 세부 디자인은 외부에서 주입할 수 있도록 구현

---
+++ FE/src/components/Box/Box.tsx
@@ -0,0 +1,24 @@
+import { theme } from '@/styles/theme';
+import { HTMLElementTypes } from '@/types/utils';
+import { css } from '@emotion/react';
+import React from 'react';
+
+type BoxProps = HTMLElementTypes<HTMLDivElement>;
+
+const Box: React.FC<BoxProps> = ({ children }, ...args) => {
+  return (
+    <div
+      css={css`
+        border-radius: 1rem;
+        box-shadow: ${theme.shadow.boxShadow};
+        width: 100%;
+        height: 100%;
+      `}
+      {...args}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Box;
