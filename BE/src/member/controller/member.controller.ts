600c04a4cff6c29c3c5ef0bee35ddd99201345b5
quiet-honey <skap0329@naver.com>
2023-11-08 02:52:39 +0900

    docs: swagger를 사용하여 회원 정보 불러오기 API 문서화

--- BE/src/member/controller/member.controller.ts
+++ BE/src/member/controller/member.controller.ts
@@ -2,13 +2,36 @@ import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { MemberService } from '../service/member.service';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { MemberResponse } from '../dto/memberResponse';
+import {
+  ApiTags,
+  ApiResponse,
+  ApiBearerAuth,
+  ApiOperation,
+  ApiHeader,
+} from '@nestjs/swagger';
 
 @Controller('/api/member')
+@ApiTags('member')
 export class MemberController {
   constructor(private memberService: MemberService) {}
 
   @Get()
   @UseGuards(AuthGuard('jwt'))
+  @ApiBearerAuth() // 문서 상에 자물쇠 아이콘을 표시하여 JWT가 필요하다는 것을 나타냄
+  @ApiOperation({
+    summary: '회원 정보를 반환하는 메서드',
+  })
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'Access Token (Bearer Token)',
+    required: true,
+  })
+  @ApiResponse({
+    status: 200,
+    description: '현재 사용자의 정보를 반환한다.',
+    type: MemberResponse,
+  })
   async getMyInfo(@Req() req: Request) {
     const token = getTokenValue(req);
     return await this.memberService.getMyInfo(token);
--- BE/src/member/dto/memberResponse.ts
+++ BE/src/member/dto/memberResponse.ts
@@ -1,10 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Member } from '../entity/member';
 
 class MemberResponse {
+  @ApiProperty({
+    example: '1',
+    description: '회원의 ID',
+    type: Number,
+  })
   private id: number;
+
+  @ApiProperty({
+    example: 'example@example.com',
+    description: '회원의 이메일',
+    type: String,
+  })
   private email: string;
+
+  @ApiProperty({
+    example: '1',
+    description: '회원의 닉네임',
+    type: String,
+  })
   private nickname: string;
+
+  @ApiProperty({
+    example: 'https://example.com',
+    description: '프로필 이미지의 주소',
+    type: String,
+  })
   private profileImg: string;
+
   constructor(member: Member) {
     this.id = member.id;
     this.email = member.email;
