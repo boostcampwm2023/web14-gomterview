0a221428e65689b99ad5978a0e287104b37fe12f
JangAJang <janghee5395@gmail.com>
2023-11-07 19:12:19 +0900

    feat: 카테고리별 게시물을 생성일자를 기준으로 정렬해 반환하는 비즈니스 로직 구현

--- BE/src/question/service/question.service.ts
+++ BE/src/question/service/question.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
-import {QuestionRepository} from "../repository/question.repository";
+import { QuestionRepository } from '../repository/question.repository';
+import { CreateQuestionRequest } from '../dto/createQuestionRequest';
+import { Member } from 'src/member/entity/member';
+import { Question } from '../entity/question';
 
 @Injectable()
 export class QuestionService {
-    constructor(private questionRepository:QuestionRepository) {
-    }
+  constructor(private questionRepository: QuestionRepository) {}
+
+  async createQuestion(
+    createQuestionRequest: CreateQuestionRequest,
+    member: Member,
+  ) {
+    const question = Question.from(createQuestionRequest, member);
+    await this.questionRepository.save(question);
+  }
+
+  async findByCategory(category: string, member: Member) {
+    return await this.questionRepository.findAllByCategoryOrderByCreatedAtDesc(
+      category,
+      member.id,
+    );
+  }
 }
