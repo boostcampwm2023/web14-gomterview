5b6eac6d23fb4e112252480d08d1a49f05019a90
Sumin <57657868+milk717@users.noreply.github.com>
2023-11-07 13:15:08 +0900

    [NDD-76] 테마 시스템에 그림자 추가 (0.3 / 1h) #10
    * feat: 테마 시스템에 shadow 추가

---
+++ FE/src/styles/_shadow.ts
@@ -0,0 +1,13 @@
+export const shadow = {
+  /**
+   * 큰 박스에 사용되는 그림자 입니다.
+   * ex) 마이페이지 프로필, 마이페이지 탭, 문제 선택기 박스
+   */
+  boxShadow: `8px 8px 25px 0px rgba(0, 0, 0, 0.10)`,
+
+  /**
+   * 작은 박스에 사용되는 그림자입니다.
+   * ex) 문제 선택기 박스 안에 있는 아이템
+   */
+  boxSmallShadow: `0px 4px 10px 0px rgba(0, 0, 0, 0.10)`,
+};
