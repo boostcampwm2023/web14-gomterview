04d641f72da0cc969a316622192f35487f8ff507
milk717 <robolindasoo@gmail.com>
2023-11-07 16:20:37 +0900

    design: 아코디언 컴포넌트 생성

---
+++ FE/src/components/foundation/Accordion/Accordion.tsx
@@ -0,0 +1,51 @@
+import React, {
+  Children,
+  cloneElement,
+  isValidElement,
+  ReactNode,
+  SyntheticEvent,
+} from 'react';
+import { css } from '@emotion/react';
+import { theme } from '@styles/theme';
+import { HTMLElementTypes } from '@/types/utils';
+
+type AccordionProps = {
+  children: ReactNode[];
+  expanded?: boolean;
+  onChange: (event: SyntheticEvent, expanded: boolean) => void;
+} & HTMLElementTypes<HTMLDivElement>;
+
+export const Accordion: React.FC<AccordionProps> = ({
+  children,
+  expanded,
+  onChange,
+  ...args
+}) => {
+  const childrenArray = Children.toArray(children).map(
+    (child) =>
+      isValidElement(child) && cloneElement(child, { ...child.props, expanded })
+  );
+
+  return (
+    <div
+      onClick={(e) => onChange(e, !!expanded)}
+      css={css`
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-self: stretch;
+        border-radius: 0.625rem;
+        box-shadow: ${expanded && theme.shadow.boxShadow};
+        cursor: pointer;
+        user-select: none;
+
+        &:hover {
+          box-shadow: ${theme.shadow.boxShadow};
+        }
+      `}
+      {...args}
+    >
+      {childrenArray}
+    </div>
+  );
+};
---
+++ FE/src/components/foundation/Accordion/AccordionSummary.tsx
@@ -0,0 +1,36 @@
+import React, { FC, ReactNode } from 'react';
+import { css, Interpolation, Theme } from '@emotion/react';
+import { HTMLElementTypes } from '@/types/utils';
+
+type AccordionSummaryProps = {
+  children: ReactNode;
+  expanded?: boolean;
+  defaultStyle?: Interpolation<Theme>;
+  expandedStyle?: Interpolation<Theme>;
+} & HTMLElementTypes<HTMLDivElement>;
+export const AccordionSummary: FC<AccordionSummaryProps> = ({
+  children,
+  expanded,
+  defaultStyle,
+  expandedStyle,
+  ...args
+}) => {
+  const optionalStyle =
+    expanded && expandedStyle ? expandedStyle : defaultStyle;
+
+  return (
+    <div
+      css={[
+        css`
+          display: flex;
+          padding: 1rem;
+          border-radius: ${expanded ? '0.625rem 0.625rem 0 0' : '0.625rem'};
+        `,
+        optionalStyle,
+      ]}
+      {...args}
+    >
+      {children}
+    </div>
+  );
+};
