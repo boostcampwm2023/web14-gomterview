684acbd78a14fab6ce42ede3e60465a6ea106d2e
이성인 <devlsi1228@gmail.com>
2023-11-07 23:57:34 +0900

    feat: route에 대한 상수화

---
+++ FE/src/constants/path.ts
@@ -0,0 +1,11 @@
+export const PATH = {
+  ROOT: '/',
+  INTERVIEW: '/interview',
+  INTERVIEW_SETTING: '/interview/setting',
+  INTERVIEW_SETTING_CONNECTION: '/interview/setting/connection',
+  INTERVIEW_SETTING_RECORD: '/interview/setting/record',
+  MYPAGE: '/mypage',
+  CONNECTION: 'connection',
+  RECORD: 'record',
+  INTERVIEW_VIDEO: `/interview/:videoId`,
+};
