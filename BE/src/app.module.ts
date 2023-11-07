8fef2b7a1b56b1dc1f6464ee7868f1358548c1a8
JangAJang <janghee5395@gmail.com>
2023-11-07 12:39:01 +0900

    feat: JWT를 구현하기 위한 모듈 추가
    Co-authored-by: quiet-honey <skap0329@naver.com>

--- BE/src/app.module.ts
+++ BE/src/app.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { MYSQL_OPTION } from './config/typeorm.config';
 import { MemberModule } from './member/member.module';
 import { AuthModule } from './auth/auth.module';
+import { JwtModule } from './jwt/jwt.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(MYSQL_OPTION), MemberModule, AuthModule],
+  imports: [TypeOrmModule.forRoot(MYSQL_OPTION), MemberModule, AuthModule, JwtModule],
   controllers: [AppController],
   providers: [AppService],
 })
