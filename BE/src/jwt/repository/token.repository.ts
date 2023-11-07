306e2443848bf18ae8e077e9c75505913b2e6c81
JangAJang <janghee5395@gmail.com>
2023-11-07 13:29:48 +0900

    feat: token을 저장하고 access token으로 조회하는 repository 구현
    Co-authored-by: quiet-honey <skap0329@naver.com>

---
+++ BE/src/jwt/repository/token.repository.ts
@@ -0,0 +1,18 @@
+import {Injectable} from "@nestjs/common";
+import {InjectRepository} from "@nestjs/typeorm";
+import {Token} from "../entity/token";
+import {Repository} from "typeorm";
+
+@Injectable()
+export class TokenRepository {
+    constructor(@InjectRepository(Token) readonly tokenRepository:Repository<Token>) {
+    }
+
+    async save(token:Token) {
+        return await this.tokenRepository.save(token);
+    }
+
+    async findByAccessToken(accessToken:string) {
+        return await this.tokenRepository.findOneBy({accessToken:accessToken});
+    }
+}
