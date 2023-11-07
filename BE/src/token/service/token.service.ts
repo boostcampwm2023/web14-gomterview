e920b1a1752d49802f86f6e3156da24d303c4eb2
JangAJang <janghee5395@gmail.com>
2023-11-07 13:50:39 +0900

    feat: 회원의 로그인 할 때, 토큰을 생성하고, access token을 반환하는 로직 구현
    Co-authored-by: quiet-honey <skap0329@naver.com>

--- BE/src/token/service/token.service.ts
+++ BE/src/token/service/token.service.ts
@@ -1,11 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import {TokenRepository} from "../repository/token.repository";
 import {MemberRepository} from "../../member/repository/member.repository";
+import {JwtService} from "@nestjs/jwt";
+import {Token} from "../entity/token";
+
+const ACCESS_TOKEN_EXPIRES_IN = "1h"; // 1 시간
+const REFRESH_TOKEN_EXPIRES_IN = "7d"; // 7 일
 
 @Injectable()
 export class TokenService {
-    constructor(readonly tokenRepository:TokenRepository, readonly memberRepository:MemberRepository, readonly ) {
+    constructor(readonly tokenRepository:TokenRepository, readonly memberRepository:MemberRepository, readonly jwtService:JwtService) {
     }
 
+    async assignToken(memberId:number) {
+        return await this.createToken(memberId);
+    }
 
+    private async createToken(memberId:number) {
+        const accessToken = await this.signToken(memberId, ACCESS_TOKEN_EXPIRES_IN);
+        const refreshToken = await this.signToken(memberId, REFRESH_TOKEN_EXPIRES_IN);
+        const token = new Token(refreshToken, accessToken);
+        await this.tokenRepository.save(token);
+        return accessToken;
+    }
+
+    private async signToken(memberId:number, expiresIn: string) {
+        return await this.jwtService.signAsync({id:memberId}, {
+            expiresIn: expiresIn,
+        });
+    }
 }
