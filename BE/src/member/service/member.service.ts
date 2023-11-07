d0f21a2fa7b4c56a5dc14a501c910fe883103c2c
quiet-honey <skap0329@naver.com>
2023-11-08 03:11:28 +0900

    feat: 사용자가 넘겨준 토큰에 의해 발생할 수 있는 커스텀 예외 처리 로직 구현
    1. 위조된 토큰이지만 guard가 이를 잡아내지 못한 경우 2. 회원이 아닌 사용자의 토큰을 사용한 경우
    위와 같은 토큰에 의해 발생할 수 있는 예외를 처리하는 로직 구현

--- BE/src/member/service/member.service.ts
+++ BE/src/member/service/member.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { MemberRepository } from '../repository/member.repository';
 import { TokenService } from 'src/token/service/token.service';
 import { MemberResponse } from '../dto/memberResponse';
+import { ManipulatedTokenNotFiltered } from 'src/token/exception/token.exception';
 
 @Injectable()
 export class MemberService {
@@ -13,6 +14,11 @@ export class MemberService {
   async getMyInfo(accessToken: string) {
     const memberId = (await this.tokenService.getPayload(accessToken)).id;
     const user = await this.memberRepository.findById(memberId);
+
+    if (!memberId || !user) {
+      throw new ManipulatedTokenNotFiltered();
+    }
+
     return new MemberResponse(user);
   }
 }
