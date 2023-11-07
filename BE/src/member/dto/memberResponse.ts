d5f6d6b6340699f812427d365d114da6f5e3b5d3
quiet-honey <skap0329@naver.com>
2023-11-08 03:01:42 +0900

    docs: 회원 정보 GET API에 관한 swagger 문서에서 필요없는 내용 제거

--- BE/src/member/dto/memberResponse.ts
+++ BE/src/member/dto/memberResponse.ts
@@ -5,28 +5,24 @@ class MemberResponse {
   @ApiProperty({
     example: '1',
     description: '회원의 ID',
-    type: Number,
   })
   private id: number;
 
   @ApiProperty({
-    example: 'example@example.com',
+    example: 'foo@example.com',
     description: '회원의 이메일',
-    type: String,
   })
   private email: string;
 
   @ApiProperty({
-    example: '1',
+    example: 'foobar',
     description: '회원의 닉네임',
-    type: String,
   })
   private nickname: string;
 
   @ApiProperty({
     example: 'https://example.com',
     description: '프로필 이미지의 주소',
-    type: String,
   })
   private profileImg: string;
 
