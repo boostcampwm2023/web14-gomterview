ad9446ababdbe6d52c1f0145a8dc16a7473a32c4
JangAJang <janghee5395@gmail.com>
2023-11-07 13:26:07 +0900

    Squashed commit of the following:
    commit 5b6eac6d23fb4e112252480d08d1a49f05019a90
    Author: Sumin <57657868+milk717@users.noreply.github.com>
    Date:   Tue Nov 7 13:15:08 2023 +0900
    
        [NDD-76] 테마 시스템에 그림자 추가 (0.3 / 1h) #10
    
        * feat: 테마 시스템에 shadow 추가
    
    commit fd35fdfdd92a960ba4e4c7c27ddb030fd5d4f821
    Author: quiet-honey <99426344+quiet-honey@users.noreply.github.com>
    Date:   Tue Nov 7 12:19:13 2023 +0900
    
        docs: 백엔드 CI툴 구현 (#5) (#7)
    
        Co-authored-by: JangAJang <99702271+JangAJang@users.noreply.github.com>
    
    commit dfc1fd93990f88d1c450b103e15b0a33ceeb43ff
    Author: Sumin <57657868+milk717@users.noreply.github.com>
    Date:   Mon Nov 6 22:25:34 2023 +0900
    
        [NDD-58] 테마 시스템 정의 (3h/3h) (#9)
    
        * feat: emotion ThemeProvider 추가하고 color, typography 추가
    
        - colorChips에 있는 색상은 프로젝트에서 바로 사용하지 않습니다.
        - colorChips에 있는 색상을 활용해서 color 시스템을 구성합니다.
    
        * feat: emotion global 스타일에 resetCss 추가
    
        * feat: emotion global 스타일에 pretendard 웹폰트로 적용
    
        * rename: resetCss -> _global 로 이름 변경
    
        * chore: 경로 별칭에 styles 디렉터리 추가
    
    commit 383fb3c5135943f9a0d58ef09bc6b30a6fc21ad1
    Author: 이성인 <seoungin1228@gmail.com>
    Date:   Mon Nov 6 16:58:38 2023 +0900
    
        feat: 상대 경로 설정 (#6)
    
    commit 2cd35c04019a95a60920f0a7929c74d0b6c3212e
    Author: YoonHaeMin <yunhatmi@gachon.ac.kr>
    Date:   Mon Nov 6 14:42:13 2023 +0900
    
        chore: release drafter 설정 추가 (#4)
    
        - be fe 모두 하나의 카테고리로 관리
        - dev 브랜치에 머지될때 실행

---
+++ BE/src/jwt/entity/token.ts
@@ -0,0 +1,17 @@
+import {BaseEntity, Column, Entity, PrimaryColumn} from "typeorm";
+
+@Entity()
+export class Token extends BaseEntity{
+
+    @PrimaryColumn()
+    readonly refreshToken:string;
+
+    @Column()
+    readonly accessToken:string;
+
+    constructor(refreshToken:string, accessToken:string) {
+        super();
+        this.refreshToken = refreshToken;
+        this.accessToken = accessToken;
+    }
+}
