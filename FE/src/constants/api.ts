9f0668c062f31330a83ecd2d62d86114046d1942
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2023-11-07 15:03:07 +0900

    fix: api 상수에서 파라미터를 선택적으로 사용하려면 ?를 추가해야함

--- FE/src/constants/api.ts
+++ FE/src/constants/api.ts
@@ -1,15 +1,15 @@
 const BASEURL = '';
 
-type Id = undefined | number;
+type Id = number;
 
 export const API = {
   MEMBER: '/member',
   VIDEO: '/video',
   VIDEO_ALL: '/video/all',
-  VIDEO_ID: (id: Id) => `/video/${id ?? ':id'}`,
+  VIDEO_ID: (id?: Id) => `/video/${id ?? ':id'}`,
   QUESTION: '/question',
-  QUESTION_ID: (id: Id) => `/question/${id ?? ':id'}`,
+  QUESTION_ID: (id?: Id) => `/question/${id ?? ':id'}`,
   ANSWER: '/answer',
   ANSWER_DEFAULT: '/answer/default',
-  ANSWER_ID: (id: Id) => `/answer/${id ?? ':id'}`,
+  ANSWER_ID: (id?: Id) => `/answer/${id ?? ':id'}`,
 };
