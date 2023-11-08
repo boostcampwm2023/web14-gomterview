ffcb9e129373370d526a8000dfce8fd5d1b0198e
JangAJang <janghee5395@gmail.com>
2023-11-08 16:28:37 +0900

    feat: Swagger에서 Api Response의 json을 반복적으로 생성하지 않게 함수 기능 추가
    Co-authored-by: quiet-honey <skap0329@naver.com>

--- BE/src/util/swagger.util.ts
+++ BE/src/util/swagger.util.ts
@@ -1,5 +1,7 @@
 import {ApiPropertyOptions} from "@nestjs/swagger/dist/decorators/api-property.decorator";
 import {Type} from "@nestjs/common";
+import {TokenResponse} from "../auth/dto/tokenResponse";
+import {ApiResponseOptions} from "@nestjs/swagger";
 
 export const createPropertyOption = (example:unknown, description: string, type: Type): ApiPropertyOptions => {
     return {
@@ -8,3 +10,12 @@ export const createPropertyOption = (example:unknown, description: string, type:
         type: type,
     } as ApiPropertyOptions;
 }
+
+export const createApiResponseOption = (status:number, description:string, type: unknown) => {
+    return {
+        status: status,
+        description: description,
+        type: type,
+    } as ApiResponseOptions;
+}
+
