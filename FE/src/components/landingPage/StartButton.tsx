5341da0f010bbd39770d94c28f38c255537a5585
milk717 <robolindasoo@gmail.com>
2023-11-09 15:36:27 +0900

    design: 랜딩페이지 시작버튼 디자인 완료

--- FE/src/components/landingPage/StartButton.tsx
+++ FE/src/components/landingPage/StartButton.tsx
@@ -1,18 +1,32 @@
 import { Link } from 'react-router-dom';
 import { PATH } from '@constants/path';
+import Button from '@foundation/Button/Button';
+import { css } from '@emotion/react';
+import { theme } from '@styles/theme';
 
 type StartButton = {
   text?: string;
 };
 
 const InterviewStartButton: React.FC<StartButton> = ({
-  text = '로그인 없이 시작하기',
+  text = '비회원으로 시작하기',
 }) => {
   // 로그인 여부에 따라 버튼의 문구가 달라집니다.
   return (
     <div>
       <Link to={PATH.INTERVIEW_SETTING}>
-        <button>{text}</button>
+        <Button
+          size="lg"
+          css={css`
+            position: relative;
+            padding: 1.5rem 3rem;
+            border-radius: 3.125rem;
+            background: ${theme.gradient.linear.blue};
+            box-shadow: ${theme.shadow.buttonLargeShadow};
+          `}
+        >
+          {text}
+        </Button>
       </Link>
     </div>
   );
