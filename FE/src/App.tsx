3d2a9e23fd2dc14c8cf048e10d1a71dbec92d7af
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2023-11-09 12:07:31 +0900

    chore: recoil 라이브러리 추가

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -3,16 +3,19 @@ import { theme } from '@styles/theme';
 import _global from '@styles/_global';
 import AppRouter from '@/AppRouter'; // AppRouter를 임포트합니다.
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RecoilRoot } from 'recoil';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <Global styles={_global} />
-        <AppRouter />
-      </ThemeProvider>
+      <RecoilRoot>
+        <ThemeProvider theme={theme}>
+          <Global styles={_global} />
+          <AppRouter />
+        </ThemeProvider>
+      </RecoilRoot>
     </QueryClientProvider>
   );
 }
