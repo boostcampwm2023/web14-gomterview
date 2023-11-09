95d40badb7daa54c379a51e802c36246904d71ca
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-11-09 21:58:22 +0900

    [NDD-97]: CORS를 위한 모듈 추가(1h/1h) (#26)
    * docs: gitignore에서 사용해야하는 CORS의 문자열 배열들을 깃에 올리지 않도록 추가
    
    * feat: cors의 설정을 처리하는 json 추가
    
    * feat: app이 CORS_CONFIG를 받아서 CORS 필터에 대한 조건을 명시하도록 기능 구현

---
+++ BE/src/config/cors.config.ts
@@ -0,0 +1,8 @@
+import {CorsOptions} from "@nestjs/common/interfaces/external/cors-options.interface";
+import {CORS_HEADERS, CORS_ORIGIN} from "./cors.secure";
+
+export const CORS_CONFIG: CorsOptions = {
+    origin: CORS_ORIGIN,
+    credentials: true,
+    exposedHeaders: CORS_HEADERS,
+}
