2be6343ddc14fdb13dde730750189bc37316f317
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-11-09 18:34:40 +0900

    [NDD-89]: Question API 리펙토링(1h / 3h) (#23)
    * refactor: swagger의 ApiPropertyOptions를 등록하는 로직을 전부 json생성하는 함수로 리펙토링
    
    * style: lint로 불필요한 import 제거
    
    * refactor: createApiResponseOption으로 컨트롤러의 swagger문서화 로직 리펙토링
    
    * chore: host를 서버에서 ip로 접근할 수 있게 .env로 이동

--- BE/src/question/dto/singleQuestionResponse.ts
+++ BE/src/question/dto/singleQuestionResponse.ts
@@ -1,23 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Question } from '../entity/question';
+import { createPropertyOption } from '../../util/swagger.util';
 
 export class SingleQuestionResponse {
-  @ApiProperty({
-    example: '1',
-    description: '게시물 ID',
-  })
+  @ApiProperty(createPropertyOption('1', '게시물 ID', Number))
   readonly id: number;
 
-  @ApiProperty({
-    example: 'CS/BE/FE/CUSTOM',
-    description: '질문에 대한 카테고리',
-  })
+  @ApiProperty(
+    createPropertyOption('CS/BE/FE/CUSTOM', '질문에 대한 카테고리', String),
+  )
   readonly category: string;
 
-  @ApiProperty({
-    example: 'CS는 무슨 단어의 약자일까요?',
-    description: '질문 내용',
-  })
+  @ApiProperty(
+    createPropertyOption('CS는 무슨 단어의 약자일까요?', '질문 내용', String),
+  )
   readonly content: string;
 
   constructor(id: number, category: string, content: string) {
