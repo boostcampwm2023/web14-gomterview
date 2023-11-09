dcbb8a916f10c72054ad32187f5447ed08776793
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2023-11-09 11:59:28 +0900

    chore: tanstack query 라이브러리 추가

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -2,13 +2,18 @@ import { Global, ThemeProvider } from '@emotion/react';
 import { theme } from '@styles/theme';
 import _global from '@styles/_global';
 import AppRouter from '@/AppRouter'; // AppRouter를 임포트합니다.
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Global styles={_global} />
-      <AppRouter />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <Global styles={_global} />
+        <AppRouter />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
