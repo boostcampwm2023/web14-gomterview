fa125165eb67af56553b45524dd939e7c75b6083
adultlee <seoungin1228@gmail.com>
2023-11-09 23:22:59 +0900

    feat: InterviewCamera 내부 state 정의

---
+++ FE/src/components/interviewPage/InterViewCamera.tsx
@@ -0,0 +1,27 @@
+import React, { useState, useRef, useEffect, useLayoutEffect } from 'react';
+import { css } from '@emotion/react';
+
+const InterviewCamera: React.FC = () => {
+  const [stream, setStream] = useState<MediaStream | null>(null);
+  const [recording, setRecording] = useState(false);
+  const [recordedBlobs, setRecordedBlobs] = useState<Blob[]>([]);
+  const [selectedMimeType, setSelectedMimeType] = useState('');
+
+  const gumVideoRef = useRef<HTMLVideoElement>(null);
+  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+  return (
+    <div
+      css={css`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 75%;
+        border: 1px solid red;
+      `}
+    >
+      면접페이지의 카메라 입니다.
+    </div>
+  );
+};
+export default InterviewCamera;
