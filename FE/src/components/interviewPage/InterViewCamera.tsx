4972b6ce114b488055464248b311938ab1ce97e6
adultlee <seoungin1228@gmail.com>
2023-11-09 23:22:59 +0900

    feat: InterviewCamera 내부 state 정의

--- FE/src/components/interviewPage/InterViewCamera.tsx
+++ FE/src/components/interviewPage/InterViewCamera.tsx
@@ -1,6 +1,14 @@
+import React, { useState, useRef, useEffect, useLayoutEffect } from 'react';
 import { css } from '@emotion/react';
 
 const InterviewCamera: React.FC = () => {
+  const [stream, setStream] = useState<MediaStream | null>(null);
+  const [recording, setRecording] = useState(false);
+  const [recordedBlobs, setRecordedBlobs] = useState<Blob[]>([]);
+  const [selectedMimeType, setSelectedMimeType] = useState('');
+
+  const gumVideoRef = useRef<HTMLVideoElement>(null);
+  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
   return (
     <div
       css={css`
