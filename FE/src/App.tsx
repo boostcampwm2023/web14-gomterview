b8ca72626ad974ee44c54f311eaab069c64f27df
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2023-11-09 21:27:49 +0900

    chore: tanstack query devtools 추가

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -4,12 +4,14 @@ import _global from '@styles/_global';
 import AppRouter from '@/AppRouter'; // AppRouter를 임포트합니다.
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RecoilRoot } from 'recoil';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <RecoilRoot>
         <ThemeProvider theme={theme}>
           <Global styles={_global} />
