ccee5fd5186719ca9adf7e48e045a5568f03f0b8
milk717 <robolindasoo@gmail.com>
2023-11-09 13:01:17 +0900

    design: Header 영역을 위한 컴포넌트 생성 후 위치만 잡음

---
+++ FE/src/components/landingPage/Header.tsx
@@ -0,0 +1,20 @@
+import { css } from '@emotion/react';
+
+const Header: React.FC = () => {
+  return (
+    <div
+      css={css`
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        padding: 1.5rem;
+      `}
+    >
+      <div>로고 들어갈 자리</div>
+      <button>구글로 시작하기</button>
+    </div>
+  );
+};
+
+export default Header;
--- FE/src/page/LandingPage/index.tsx
+++ FE/src/page/LandingPage/index.tsx
@@ -1,10 +1,12 @@
 import LandingPageLayout from '@/components/landingPage/Layout';
 import SideBackground from '@/components/landingPage/SideBackground';
 import Intro from '@/components/landingPage/Intro';
+import Header from '@components/landingPage/Header';
 
 const RenderingPage: React.FC = () => {
   return (
     <LandingPageLayout>
+      <Header />
       <SideBackground />
       <Intro />
     </LandingPageLayout>
