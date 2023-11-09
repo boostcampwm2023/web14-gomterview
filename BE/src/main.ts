35cb8e68d5e2429c3a278212815bbe9bbbc7def1
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-11-09 21:58:22 +0900

    [NDD-97]: CORS를 위한 모듈 추가(1h/1h) (#26)
    * docs: gitignore에서 사용해야하는 CORS의 문자열 배열들을 깃에 올리지 않도록 추가
    
    * feat: cors의 설정을 처리하는 json 추가
    
    * feat: app이 CORS_CONFIG를 받아서 CORS 필터에 대한 조건을 명시하도록 기능 구현

--- BE/src/main.ts
+++ BE/src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { setupSwagger } from './config/swagger.config';
+import {CORS_CONFIG} from "./config/cors.config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors(CORS_CONFIG);
   setupSwagger(app);
   await app.listen(8080);
 }
