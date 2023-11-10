871c2636e51820323614a3b119447b6f45aa6cc4
quiet-honey <skap0329@naver.com>
2023-11-10 17:36:02 +0900

    test: Member API E2E 테스트 코드 작성
    1. /api/member GET API 에 대한 성공 테스트
    2. /api/member GET API에서 유효하지 않은 토큰 사용으로 인한 실패 테스트
    위의 2가지의 경우에 대한 테스트 코드 작성

--- BE/src/member/controller/member.controller.spec.ts
+++ BE/src/member/controller/member.controller.spec.ts
@@ -1,9 +1,14 @@
-import { Test } from '@nestjs/testing';
+import { Test, TestingModule } from '@nestjs/testing';
 import { MemberController } from './member.controller';
 import { MemberResponse } from '../dto/memberResponse';
 import { Request } from 'express';
 import { Member } from '../entity/member';
 import { ManipulatedTokenNotFiltered } from 'src/token/exception/token.exception';
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from 'src/app.module';
+import * as request from 'supertest';
+import { TokenModule } from 'src/token/token.module';
+import { TokenService } from 'src/token/service/token.service';
 
 describe('MemberController', () => {
   let memberController: MemberController;
@@ -45,5 +50,46 @@ describe('MemberController', () => {
     expect(() =>
       memberController.getMyInfo(mockReq as unknown as Request),
     ).toThrow(ManipulatedTokenNotFiltered);
+  });
+});
+
+describe('MemberController (E2E Test)', () => {
+  let app: INestApplication;
+  let tokenService: TokenService;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule, TokenModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+
+    tokenService = moduleFixture.get<TokenService>(TokenService);
+  });
+
+  it('GET /api/member (회원 정보 반환 성공)', async () => {
+    const memberId = 1; // 항상 DB에 들어있는 회원의 ID로의 설정이 필요해보입니다.
+    const validToken = await tokenService.assignToken(memberId);
+
+    const response = await request(app.getHttpServer())
+      .get('/api/member')
+      .set('Authorization', `Bearer ${validToken}`)
+      .expect(200);
+
+    expect(response.body.id).toBe(memberId);
+  });
+
+  it('GET /api/member (유효하지 않은 토큰 사용으로 인한 회원 정보 반환 실패)', async () => {
+    const invalidToken = 'INVALID_TOKEN';
+
+    await request(app.getHttpServer())
+      .get('/api/member')
+      .set('Authorization', `Bearer ${invalidToken}`)
+      .expect(401);
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
