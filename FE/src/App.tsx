2e4c48fdc51c228782cef776997f708589bcd438
YoonHaeMin <yunhatmi@gachon.ac.kr>
2023-11-10 22:42:15 +0900

    [NDD-86] 상태관리 라이브러리 추가 (순수 세팅 시간 1h/8h) (상태 관리 고민시간 4h/???) (#25)
    * chore: axios 라이브러리 추가
    
    * chore: axios 기본 instance 생성
    
    * chore: tanstack query 라이브러리 추가
    
    * chore: recoil 라이브러리 추가
    
    * chore: tanstack query devtools 추가

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -2,13 +2,23 @@ import { Global, ThemeProvider } from '@emotion/react';
 import { theme } from '@styles/theme';
 import _global from '@styles/_global';
 import AppRouter from '@/AppRouter'; // AppRouter를 임포트합니다.
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RecoilRoot } from 'recoil';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Global styles={_global} />
-      <AppRouter />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <RecoilRoot>
+        <ThemeProvider theme={theme}>
+          <Global styles={_global} />
+          <AppRouter />
+        </ThemeProvider>
+      </RecoilRoot>
+    </QueryClientProvider>
   );
 }
 
