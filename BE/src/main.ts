416a5e35abda99f7475b424c642b683350a8c605
quiet-honey <skap0329@naver.com>
2023-11-11 22:36:38 +0900

    chore: JWT를 쿠키에 넣어 사용하기 위해 cookieParser 미들웨어 추가

--- BE/src/main.ts
+++ BE/src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { setupSwagger } from './config/swagger.config';
 import { CORS_CONFIG } from './config/cors.config';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors(CORS_CONFIG);
   setupSwagger(app);
