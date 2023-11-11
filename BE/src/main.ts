bf4c6c53b5ca469a839e10de886a52f4bd855e99
quiet-honey <99426344+quiet-honey@users.noreply.github.com>
2023-11-11 18:15:02 +0900

    [NDD-103] Member API E2E 테스트 (1h / 1h) (#29)
    * test: Member API E2E 테스트 코드 작성
    
    1. /api/member GET API 에 대한 성공 테스트
    2. /api/member GET API에서 유효하지 않은 토큰 사용으로 인한 실패 테스트
    위의 2가지의 경우에 대한 테스트 코드 작성
    
    * style: lint 적용
    
    * test: 인수 테스트 코드 로직 변경 완료
    
    GET /api/member에 대한 E2E 테스트 완료
    
    * style: Window 개행문자 관련 에러 해결을 위한 lint 설정 추가
    
    * feat: TestingModule을 생성하는 Util 메서드 구현

--- BE/src/main.ts
+++ BE/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { setupSwagger } from './config/swagger.config';
-import {CORS_CONFIG} from "./config/cors.config";
+import { CORS_CONFIG } from './config/cors.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
