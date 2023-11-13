01b5d42a344fbcca5ae15ff34dd1005dad35c629
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2023-11-13 17:15:44 +0900

    feat: setting 전역 상태 생성

---
+++ FE/src/atoms/interviewSetting.ts
@@ -0,0 +1,43 @@
+import { atom } from 'recoil';
+
+type PageStatus = 'pending' | 'success' | 'error';
+type RecordMethod = 'local' | 'idrive' | 'none' | undefined;
+
+type SelectedData = {
+  [key: string]: {
+    id: number;
+    content: string;
+    answer: string;
+  };
+};
+
+export const questionSetting = atom<{
+  status: PageStatus;
+  selectedData: SelectedData;
+}>({
+  key: 'questionSetting',
+  default: {
+    status: 'pending',
+    selectedData: {},
+  },
+});
+
+export const videoSetting = atom<{
+  status: PageStatus;
+}>({
+  key: 'videoSetting',
+  default: {
+    status: 'pending',
+  },
+});
+
+export const recordSetting = atom<{
+  status: PageStatus;
+  method: RecordMethod;
+}>({
+  key: 'recordSetting',
+  default: {
+    status: 'pending',
+    method: undefined,
+  },
+});
