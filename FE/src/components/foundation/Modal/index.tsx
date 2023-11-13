5c04759c6d59dd54751b312db56c4a6032a3e989
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2023-11-13 13:06:49 +0900

    refactor: modal 컴포넌트 조합을 재설정
    - modal 사용 방법을 modal.laout에서 modal로 변경

--- FE/src/components/foundation/Modal/index.tsx
+++ FE/src/components/foundation/Modal/index.tsx
@@ -3,11 +3,15 @@ import ModalFooter from './ModalFooter';
 import ModalHeader from './ModalHeader';
 import ModalLayout from './ModalLayout';
 
-const Modal = {
-  header: ModalHeader,
-  content: ModalContent,
-  footer: ModalFooter,
-  layout: ModalLayout,
+type ModalComponent = typeof ModalLayout & {
+  header: typeof ModalHeader;
+  content: typeof ModalContent;
+  footer: typeof ModalFooter;
 };
+const Modal: ModalComponent = ModalLayout as ModalComponent;
+
+Modal.header = ModalHeader;
+Modal.content = ModalContent;
+Modal.footer = ModalFooter;
 
 export default Modal;
