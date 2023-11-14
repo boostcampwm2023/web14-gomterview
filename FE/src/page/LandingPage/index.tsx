ce986908145c4e8c2c7334e98def969534cca8bb
milk717 <robolindasoo@gmail.com>
2023-11-14 16:32:44 +0900

    feat: 구글 로그인 페이지로 리다이랙션 하는 기능 구현

--- FE/src/page/LandingPage/index.tsx
+++ FE/src/page/LandingPage/index.tsx
@@ -6,9 +6,13 @@ import GoogleLoginButton from '@components/landingPage/GoogleLoginButton';
 import { css } from '@emotion/react';
 import { API } from '@constants/api';
 
+//TODO 로그인을 제외한 모든 api는 아직 MSW를 이용하고 있기 때문에 이곳에 임시로 정의했습니다.
+const BASE_URL =
+  'http://ec2-3-39-187-198.ap-northeast-2.compute.amazonaws.com:8080/api';
+
 const LandingPage: React.FC = () => {
   const handleGoogleLogin = () => {
-    window.location.href = `${API.LOGIN}`;
+    window.location.href = `${BASE_URL}${API.LOGIN}`;
   };
 
   return (
