28f1cf6ac068772e6293f840c11d43315251f3a4
adultlee <seoungin1228@gmail.com>
2023-11-15 23:55:05 +0900

    feat: InterviewCamera 컴포넌트 개발
    1. 최소한의 반응형을 지원할것,
    2. height를 고정후 width를 변화시키지만 over-flow : hidden 속성으로 x축 스크롤 방지
    3. Question div 와  Answer Div 에 대한 기능 구현

--- FE/src/components/interviewPage/InterviewCamera.tsx
+++ FE/src/components/interviewPage/InterviewCamera.tsx
@@ -1,140 +1,105 @@
-import React, { useState, useRef, useEffect, useLayoutEffect } from 'react';
 import { css } from '@emotion/react';
+import { RefObject } from 'react';
 
-const InterviewCamera: React.FC = () => {
-  const [stream, setStream] = useState<MediaStream | null>(null);
-  const [recording, setRecording] = useState(false);
-  const [recordedBlobs, setRecordedBlobs] = useState<Blob[]>([]);
-  const [selectedMimeType, setSelectedMimeType] = useState('');
+type InterviewCameraProps = {
+  mirrorVideoRef: RefObject<HTMLVideoElement>;
+  isScriptInView: boolean;
+  question: string;
+  answer: string;
+};
 
-  const mirrorVideoRef = useRef<HTMLVideoElement>(null);
-  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
-
-  useLayoutEffect(() => {
-    const mimeTypes = getSupportedMimeTypes();
-    if (mimeTypes.length > 0) {
-      setSelectedMimeType(mimeTypes[0]);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (!stream) {
-      void getMedia();
-    }
-
-    return () => {
-      if (stream) {
-        stream.getTracks().forEach((track) => track.stop());
-      }
-    };
-  }, [stream]);
-
-  const getMedia = async () => {
-    try {
-      const constraints = {
-        audio: {
-          echoCancellation: { exact: true },
-        },
-        video: {
-          width: 1280,
-          height: 720,
-        },
-      };
-      const mediaStream =
-        await navigator.mediaDevices.getUserMedia(constraints);
-      setStream(mediaStream);
-      if (mirrorVideoRef.current) {
-        mirrorVideoRef.current.srcObject = mediaStream;
-      }
-    } catch (e) {
-      console.log(`현재 마이크와 카메라가 연결되지 않았습니다`);
-    }
-  };
-
-  const handleStartRecording = () => {
-    setRecordedBlobs([]);
-    try {
-      mediaRecorderRef.current = new MediaRecorder(stream as MediaStream, {
-        mimeType: selectedMimeType,
-      });
-      mediaRecorderRef.current.ondataavailable = (event) => {
-        if (event.data && event.data.size > 0) {
-          setRecordedBlobs((prev) => [...prev, event.data]);
-        }
-      };
-      mediaRecorderRef.current.start();
-      setRecording(true);
-    } catch (e) {
-      console.log(`MediaRecorder error`);
-    }
-  };
-
-  const handleStopRecording = () => {
-    if (mediaRecorderRef.current) {
-      mediaRecorderRef.current.stop();
-    }
-    setRecording(false);
-  };
-
-  const handleDownload = () => {
-    const blob = new Blob(recordedBlobs, { type: selectedMimeType });
-    const url = window.URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.style.display = 'none';
-    a.href = url;
-    a.download = 'recorded.webm';
-    document.body.appendChild(a);
-    a.click();
-    setTimeout(() => {
-      document.body.removeChild(a);
-      window.URL.revokeObjectURL(url);
-    }, 100);
-  };
-
-  const getSupportedMimeTypes = () => {
-    const types = [
-      'video/webm; codecs=vp8',
-      'video/webm; codecs=vp9',
-      'video/webm; codecs=h264',
-      'video/mp4; codecs=h264',
-    ];
-    return types.filter((type) => MediaRecorder.isTypeSupported(type));
-  };
-
+const InterviewCamera: React.FC<InterviewCameraProps> = ({
+  mirrorVideoRef,
+  isScriptInView,
+  question,
+  answer,
+}) => {
   return (
     <div
       css={css`
         display: flex;
-        flex-direction: column;
+        position: relative;
+        justify-content: center;
+        height: 100%;
         align-items: center;
         background-color: black;
+        overflow: hidden;
       `}
     >
+      <div
+        css={css`
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          position: absolute;
+          top: 3.125rem;
+          left: 50%;
+          transform: translateX(-50%);
+          z-index: 1;
+          width: 62.5rem;
+          height: 5rem;
+          background-color: black;
+          color: white;
+          opacity: 60%;
+          border-radius: 0rem 0rem 2rem 2rem;
+
+          @media (max-width: 1000px) {
+            width: 80%;
+          }
+
+          @media (max-width: 500px) {
+            width: 90%;
+          }
+        `}
+      >
+        {question}
+      </div>
       <video
         ref={mirrorVideoRef}
-        playsInline
         autoPlay
         muted
         css={css`
-          width: 100%;
-          height: 80%;
+          height: 100%;
           transform: scaleX(-1);
         `}
       />
+      <div
+        css={css`
+          display: ${isScriptInView ? 'flex' : 'none'};
+          justify-content: center;
+          align-items: center;
+          position: absolute;
+          bottom: 0;
+          left: 50%;
+          transform: translateX(-50%);
+          z-index: 1;
+          width: 62.5rem;
+          height: 11.5rem;
+          background-color: black;
+          color: white;
+          opacity: 60%;
+          border-radius: 2rem 2rem 0rem 0rem;
+          padding: 1.25rem;
 
-      <div>
-        <button onClick={handleStartRecording} disabled={recording}>
-          시작
-        </button>
-        <button onClick={handleStopRecording} disabled={!recording}>
-          종료
-        </button>
-        <button
-          onClick={handleDownload}
-          disabled={recording || recordedBlobs.length === 0}
+          @media (max-width: 1000px) {
+            width: 80%;
+          }
+
+          @media (max-width: 500px) {
+            width: 90%;
+            height: 14rem;
+          }
+        `}
+      >
+        <div
+          css={css`
+            width: 100%;
+            height: 100%;
+            overflow-y: scroll;
+          `}
         >
-          저장
-        </button>
+          {answer}
+        </div>
       </div>
     </div>
   );
