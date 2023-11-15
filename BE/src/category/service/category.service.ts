0e8b58cda4f73adbab82276000911a8ab7faaac2
JangAJang <janghee5395@gmail.com>
2023-11-15 15:37:00 +0900

    refactor: 비즈니스 로직에서 회원을 검증하는 로직을 token.util의 메서드 재사용하게 수정

--- BE/src/category/service/category.service.ts
+++ BE/src/category/service/category.service.ts
@@ -4,8 +4,8 @@ import { CreateCategoryRequest } from '../dto/createCategoryRequest';
 import { Member } from '../../member/entity/member';
 import { isEmpty } from 'class-validator';
 import { CategoryNameEmptyException } from '../exception/category.exception';
-import { ManipulatedTokenNotFiltered } from '../../token/exception/token.exception';
 import { Category } from '../entity/category';
+import { validateManipulatedToken } from 'src/util/token.util';
 
 @Injectable()
 export class CategoryService {
@@ -15,12 +15,10 @@ export class CategoryService {
     createCategoryRequest: CreateCategoryRequest,
     member: Member,
   ) {
+    validateManipulatedToken(member);
+
     if (isEmpty(createCategoryRequest.name)) {
       throw new CategoryNameEmptyException();
-    }
-
-    if (isEmpty(member)) {
-      throw new ManipulatedTokenNotFiltered();
     }
 
     return await this.categoryRepository.save(
