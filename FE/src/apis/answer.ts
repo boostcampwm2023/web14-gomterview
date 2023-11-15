3ac5e395a2c47cb3560050579e31ca84448ebb8c
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2023-11-15 13:19:05 +0900

    feat: getAPIResponseData 유틸함수 추가

--- FE/src/apis/answer.ts
+++ FE/src/apis/answer.ts
@@ -1,18 +1,18 @@
 import { Answer } from '@/types/answer';
-import api from './axios';
 import { API } from '@/constants/api';
+import getAPIResponseData from '@/utils/getAPIResponseData';
 
 export const getQuestionAnswer = async (questionId: number) => {
-  const response = await api.get<Answer[]>(API.ANSWER_ID(questionId));
-
-  return response.data;
+  return await getAPIResponseData<Answer[]>({
+    method: 'get',
+    url: API.ANSWER_ID(questionId),
+  });
 };
 
 export const postAnswer = async (questionId: number, content: string) => {
-  const response = await api.post<void>(API.ANSWER, {
-    questionId,
-    content,
+  return await getAPIResponseData({
+    method: 'post',
+    url: API.ANSWER,
+    data: { questionId, content },
   });
-
-  return response.data;
 };
