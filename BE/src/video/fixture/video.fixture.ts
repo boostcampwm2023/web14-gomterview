50828b9edb864a37f1a27c7f7a8de01d5a762f55
quiet-honey <skap0329@naver.com>
2023-11-16 01:30:42 +0900

    refactor: video 전체 조회 API의 Swagger 문서 상에서 사용하는 예시 내용을 video.fixture.ts로 이동

---
+++ BE/src/video/fixture/video.fixture.ts
@@ -0,0 +1,26 @@
+export const videoListExample = [
+  {
+    id: 5,
+    thumbnail: 'https://test-thumbnail1.com',
+    videoName: 'test1.webm',
+    videoLength: '02:42',
+    isPublic: false,
+    createdAt: 1699983079205,
+  },
+  {
+    id: 4,
+    thumbnail: 'https://test-thumbnail2.com',
+    videoName: 'test2.webm',
+    videoLength: '03:29',
+    isPublic: false,
+    createdAt: 1699983079201,
+  },
+  {
+    id: 3,
+    thumbnail: 'https://test-thumbnail3.com',
+    videoName: 'test.mp4',
+    videoLength: '05:22',
+    isPublic: false,
+    createdAt: 1699858790176,
+  },
+];
