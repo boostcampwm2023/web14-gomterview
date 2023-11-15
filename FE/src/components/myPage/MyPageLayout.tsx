1fab6c5f90445891352644db3e0294ef1f5d2a3c
milk717 <robolindasoo@gmail.com>
2023-11-15 23:22:21 +0900

    design: 로고가 좌측 상단에 위치하도록 레이아웃 수정

--- FE/src/components/myPage/MyPageLayout.tsx
+++ FE/src/components/myPage/MyPageLayout.tsx
@@ -1,5 +1,7 @@
 import Layout from '@/components/layout/Layout';
 import { css } from '@emotion/react';
+import MyPageHeader from '@components/myPage/MyPageHeader';
+import Logo from '@common/Logo/Logo';
 
 type MyPageLayoutProps = {
   children: React.ReactNode;
@@ -7,16 +9,26 @@ type MyPageLayoutProps = {
 
 const MyPageLayout: React.FC<MyPageLayoutProps> = ({ children }) => {
   return (
-    <Layout
-      direction="column"
-      css={css`
-        height: auto;
-        padding: 1rem 0;
-        row-gap: 1.5rem;
-      `}
-    >
-      {children}
-    </Layout>
+    <div>
+      <div
+        css={css`
+          padding: 1.5rem;
+        `}
+      >
+        <Logo />
+      </div>
+      <Layout
+        direction="column"
+        css={css`
+          height: auto;
+          padding: 1rem 0;
+          row-gap: 1.5rem;
+        `}
+      >
+        <MyPageHeader />
+        {children}
+      </Layout>
+    </div>
   );
 };
 
--- FE/src/page/myPage/index.tsx
+++ FE/src/page/myPage/index.tsx
@@ -1,14 +1,10 @@
 import MyPageLayout from '@/components/myPage/MyPageLayout';
-import MyPageHeader from '@/components/myPage/MyPageHeader';
 import Profile from '@/components/myPage/Profile';
-import Logo from '@common/Logo/Logo';
 import MyPagesTabs from '@components/myPage/MyPagesTabs';
 
 const MyPage: React.FC = () => {
   return (
     <MyPageLayout>
-      <Logo />
-      <MyPageHeader />
       <Profile />
       <MyPagesTabs />
     </MyPageLayout>
