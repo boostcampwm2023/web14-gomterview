c8db3c8bc71c981a1d8d2177c989f8cc36110d3b
milk717 <robolindasoo@gmail.com>
2023-11-16 15:36:32 +0900

    feat: idrive에 있는 url로 동영상 변경

--- FE/src/page/interviewVideoPage/index.tsx
+++ FE/src/page/interviewVideoPage/index.tsx
@@ -6,7 +6,7 @@ const InterviewVideoPage: React.FC = () => {
   const dummyData = {
     videoName: '비디오 이름',
     date: '2001.07.17',
-    url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
+    url: 'https://u2e0.c18.e2-4.dev/videos/%EB%A3%A8%EC%9D%B4%EB%B7%94%ED%86%B5%ED%86%B5%ED%8A%80%EA%B8%B0%EB%84%A4_test_07ab3e8a-1a0a-453f-8d60-afacb57b0075.webm',
   };
 
   return (
