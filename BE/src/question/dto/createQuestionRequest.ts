5faaf02226b92dc7fd065010406a471618b095fc
JangAJang <janghee5395@gmail.com>
2023-11-20 17:31:52 +0900

    refactor: dto에 타입 검증 데코레이터 추가

--- BE/src/question/dto/createQuestionRequest.ts
+++ BE/src/question/dto/createQuestionRequest.ts
@@ -1,5 +1,13 @@
+import { IsNotEmpty, IsString } from '@nestjs/class-validator';
+import { IsNumber } from 'class-validator';
+
 export class CreateQuestionRequest {
+  @IsNotEmpty()
+  @IsNumber()
   categoryId: number;
+
+  @IsNotEmpty()
+  @IsString()
   content: string;
 
   constructor(categoryId: number, content: string) {
