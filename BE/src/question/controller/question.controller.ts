a59febf1e59ee8f33af0f175254194a94f575f26
JangAJang <janghee5395@gmail.com>
2023-11-20 15:30:28 +0900

    feat: 컨트롤러 로직 구현

--- BE/src/question/controller/question.controller.ts
+++ BE/src/question/controller/question.controller.ts
@@ -1,7 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { QuestionService } from '../service/question.service';
+import { CreateQuestionRequest } from '../dto/createQuestionRequest';
+import { AuthGuard } from '@nestjs/passport';
+import {
+  ApiBody,
+  ApiCookieAuth,
+  ApiOperation,
+  ApiResponse,
+} from '@nestjs/swagger';
+import { createApiResponseOption } from '../../util/swagger.util';
+import { QuestionResponse } from '../dto/questionResponse';
 
 @Controller('question')
 export class QuestionController {
   constructor(private questionService: QuestionService) {}
+
+  @Post()
+  @UseGuards(AuthGuard('jwt'))
+  @ApiCookieAuth()
+  @ApiBody({ type: CreateQuestionRequest })
+  @ApiOperation({
+    summary: '커스텀 질문 저장',
+  })
+  @ApiResponse(
+    createApiResponseOption(201, '커스텀 질문 저장 완료', QuestionResponse),
+  )
+  async createCustomQuestion(
+    @Body() createQuestionRequest: CreateQuestionRequest,
+  ) {
+    return await this.questionService.createQuestion(createQuestionRequest);
+  }
 }
