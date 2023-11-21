c1f4eb966f9e6ffc1948e28a7bb0bde58e6d0847
JangAJang <janghee5395@gmail.com>
2023-11-21 13:56:59 +0900

    docs: dto에 대한 문서화 코드 추가

--- BE/src/question/dto/questionResponse.ts
+++ BE/src/question/dto/questionResponse.ts
@@ -1,9 +1,19 @@
 import { Question } from '../entity/question';
+import { ApiProperty } from '@nestjs/swagger';
+import { createPropertyOption } from '../../util/swagger.util';
 
 export class QuestionResponse {
+  @ApiProperty(createPropertyOption(1, '질문의 ID', Number))
   questionId: number;
+  @ApiProperty(
+    createPropertyOption('이장희는 누구인가요?', '질문 내용', String),
+  )
   questionContent: string;
+  @ApiProperty(createPropertyOption(1, '대표답변의 ID', Number))
   answerId: number;
+  @ApiProperty(
+    createPropertyOption('존잘 백엔드 캠퍼!', '대표답변 내용', String),
+  )
   answerContent: string;
 
   constructor(
