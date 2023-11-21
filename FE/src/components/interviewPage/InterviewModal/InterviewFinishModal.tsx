afd351756782a7239ffa2e821f27c21b06c97497
adultlee <seoungin1228@gmail.com>
2023-11-21 11:40:14 +0900

    feat: Debounce를 통한 렌더링 최적화

--- FE/src/components/interviewPage/InterviewModal/InterviewFinishModal.tsx
+++ FE/src/components/interviewPage/InterviewModal/InterviewFinishModal.tsx
@@ -29,17 +29,28 @@ const InterviewFinishModal: React.FC<InterviewFinishModalProps> = ({
   });
 
   useEffect(() => {
+    let timeoutId: number | undefined;
+
     const handleResize = () => {
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
+      window.clearTimeout(timeoutId);
+
+      timeoutId = window.setTimeout(() => {
+        console.log({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      }, 250);
     };
 
     window.addEventListener('resize', handleResize);
 
     return () => {
       window.removeEventListener('resize', handleResize);
+      if (timeoutId !== undefined) window.clearTimeout(timeoutId);
     };
   }, []);
 
