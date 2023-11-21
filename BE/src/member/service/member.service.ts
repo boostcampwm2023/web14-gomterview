998d58e14ae1dfd9896935215e0aede808b83b4a
quiet-honey <skap0329@naver.com>
2023-11-21 14:31:28 +0900

    feat: 지정된 회사 중 랜덤한 회사 하나의 이름을 prefix로 붙여 반환하는 메서드 구현

--- BE/src/member/service/member.service.ts
+++ BE/src/member/service/member.service.ts
@@ -5,6 +5,21 @@ import { MemberRepository } from '../repository/member.repository';
 import { getTokenValue } from 'src/util/token.util';
 import { MemberNicknameResponse } from '../dto/memberNicknameResponse';
 
+const companies = [
+  '네이버',
+  '카카오',
+  '라인',
+  '쿠팡',
+  '우아한형제들',
+  '당근',
+  '비바리퍼블리카',
+  'Microsoft',
+  'Apple',
+  'Google',
+  'Amazon',
+  'Meta',
+];
+
 @Injectable()
 export class MemberService {
   constructor(
@@ -12,7 +27,8 @@ export class MemberService {
     private memberRepository: MemberRepository,
   ) {}
   async getNameForInterview(req: Request) {
-    if (!req.cookies['accessToken']) return '면접자';
+    if (!req.cookies['accessToken'])
+      return new MemberNicknameResponse(`면접자`);
 
     // TODO: 추후에 랜덤 Prefix 생성할 필요가 있음
     return new MemberNicknameResponse(
@@ -20,8 +36,14 @@ export class MemberService {
     );
   }
 
-  async getMemberByToken(tokenValue: string) {
+  private async getMemberByToken(tokenValue: string) {
     const memberId = (await this.tokenService.getPayload(tokenValue)).id;
     return await this.memberRepository.findById(memberId);
+  }
+
+  private getNameWithPrefix(nickname: string) {
+    const randomCompany =
+      companies[Math.floor(Math.random() * companies.length)];
+    return `${randomCompany} 최종 면접에 들어온 ${nickname}`;
   }
 }
