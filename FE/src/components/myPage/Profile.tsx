fb5a9f5195f38cd834df25d9ce7c579145aec47b
Sumin <robolindasoo@gmail.com>
2023-11-22 20:49:02 +0900

    feat: userInfo가 없는 경우 랜딩으로 리다이랙션

--- FE/src/components/myPage/Profile.tsx
+++ FE/src/components/myPage/Profile.tsx
@@ -5,10 +5,14 @@ import Typography from '../foundation/Typography/Typography';
 import { QUERY_KEY } from '@constants/queryKey';
 import { useQueryClient } from '@tanstack/react-query';
 import { User } from '@/types/user';
+import { Navigate } from 'react-router-dom';
+import { PATH } from '@constants/path';
 
 const Profile: React.FC = () => {
   const queryClient = useQueryClient();
   const userInfo = queryClient.getQueryData<User>(QUERY_KEY.MEMBER);
+
+  if (!userInfo) return <Navigate to={PATH.ROOT} />;
 
   return (
     <Box
@@ -21,9 +25,9 @@ const Profile: React.FC = () => {
         padding: 1.5rem;
       `}
     >
-      <Avatar src={userInfo?.profileImg} />
-      <Typography variant="title3">{userInfo?.nickname}</Typography>
-      <Typography variant="body3">{userInfo?.email}</Typography>
+      <Avatar src={userInfo.profileImg} />
+      <Typography variant="title3">{userInfo.nickname}</Typography>
+      <Typography variant="body3">{userInfo.email}</Typography>
     </Box>
   );
 };
