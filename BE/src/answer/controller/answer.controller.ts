90a695e4a6b307c43f2f6e1ca59edd7b147bb51d
JangAJang <janghee5395@gmail.com>
2023-11-22 14:29:35 +0900

    feat: 컨트롤러 테스트를 위한 로직 구현

--- BE/src/answer/controller/answer.controller.ts
+++ BE/src/answer/controller/answer.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  ApiBody,
+  ApiCookieAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { AnswerService } from '../service/answer.service';
+import { CreateAnswerRequest } from '../dto/createAnswerRequest';
+import { Request } from 'express';
+import { AuthGuard } from '@nestjs/passport';
+import { createApiResponseOption } from '../../util/swagger.util';
+import { Member } from '../../member/entity/member';
+import { AnswerResponse } from '../dto/answerResponse';
 
-@Controller('answer')
-export class AnswerController {}
+@ApiTags('answer')
+@Controller('/api/answer')
+export class AnswerController {
+  constructor(private answerService: AnswerService) {}
+
+  @Post()
+  @UseGuards(AuthGuard('jwt'))
+  @ApiCookieAuth()
+  @ApiBody({ type: CreateAnswerRequest })
+  @ApiOperation({
+    summary: '질문에 새로운 답변 추가',
+  })
+  @ApiResponse(createApiResponseOption(201, '답변 생성 완료', AnswerResponse))
+  async createAnswer(
+    @Body() createAnswerRequest: CreateAnswerRequest,
+    @Req() req: Request,
+  ) {
+    return await this.answerService.addAnswer(
+      createAnswerRequest,
+      req.user as Member,
+    );
+  }
+}
