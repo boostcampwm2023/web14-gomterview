23c545fa082ac14012fe8a081593b33909782b03
JangAJang <janghee5395@gmail.com>
2023-11-22 20:54:56 +0900

    refactor: 응답 반환시에 나만의 질문 카테고리를 리스트에서 분리해 반환

--- BE/src/category/dto/categoryListResponse.ts
+++ BE/src/category/dto/categoryListResponse.ts
@@ -1,9 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { createPropertyOption } from '../../util/swagger.util';
 import { CategoryResponse } from './categoryResponse';
-import { categoryListResponseFixture } from '../fixture/category.fixture';
+import {
+  categoryListResponseFixture,
+  customCategoryFixture,
+} from '../fixture/category.fixture';
 
 export class CategoryListResponse {
+  @ApiProperty(
+    createPropertyOption(
+      CategoryResponse.from(customCategoryFixture),
+      '커스텀 카테고리 정보',
+      CategoryResponse,
+    ),
+  )
+  customCategory: CategoryResponse | null;
+
   @ApiProperty(
     createPropertyOption(categoryListResponseFixture, '카테고리 리스트 응답', [
       CategoryResponse,
@@ -11,11 +23,24 @@ export class CategoryListResponse {
   )
   categoryList: CategoryResponse[];
 
-  constructor(categoryList: CategoryResponse[]) {
+  constructor(
+    customCategory: CategoryResponse,
+    categoryList: CategoryResponse[],
+  ) {
+    this.customCategory = customCategory;
     this.categoryList = categoryList;
   }
 
   static of(categoryList: CategoryResponse[]) {
-    return new CategoryListResponse(categoryList);
+    const customCategory = categoryList.filter(
+      (each) => each.name == '나만의 질문',
+    )[0];
+
+    const others = categoryList.filter((each) => each.name != '나만의 질문');
+
+    return new CategoryListResponse(
+      customCategory ? customCategory : null,
+      others,
+    );
   }
 }
