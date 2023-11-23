a60a94046b989803d9a37532da0b69c9dcca4580
Sumin <robolindasoo@gmail.com>
2023-11-23 15:06:58 +0900

    feat: 유저 면접용 이름 가져오는 MSW 추가

--- FE/src/mocks/handlers/member.ts
+++ FE/src/mocks/handlers/member.ts
@@ -1,5 +1,5 @@
 import { API } from '@/constants/api';
-import { HttpResponse, http } from 'msw';
+import { http, HttpResponse } from 'msw';
 
 const memberHandlers = [
   http.get(API.MEMBER, () => {
@@ -9,6 +9,11 @@ const memberHandlers = [
       nickname: 'admin',
       profileImg:
         'https://us.123rf.com/450wm/tuktukdesign/tuktukdesign1705/tuktukdesign170500052/77461551-%EB%82%A8%EC%9E%90-%EC%82%AC%EC%9A%A9%EC%9E%90-%EC%95%84%EC%9D%B4%EC%BD%98-%EC%82%AC%EB%9E%8C-%ED%94%84%EB%A1%9C%ED%95%84-%EC%95%84%EB%B0%94%ED%83%80-%EA%B8%80%EB%A6%AC%ED%94%84-%EB%B2%A1%ED%84%B0-%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8.jpg',
+    });
+  }),
+  http.get(API.MEMBER_NAME, () => {
+    return HttpResponse.json({
+      nickname: '토스에서 모셔간 이성인',
     });
   }),
 ];
