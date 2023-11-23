7bb7de6e3aa17d9966fd07b825d8bcf6533291f4
Sumin <robolindasoo@gmail.com>
2023-11-23 22:11:28 +0900

    hotfix: 마이페이지에서 문제 선택 안되는 문제 수정

--- FE/src/page/myPage/index.tsx
+++ FE/src/page/myPage/index.tsx
@@ -1,17 +1,18 @@
 import MyPageLayout from '@/components/myPage/MyPageLayout';
 import Profile from '@/components/myPage/Profile';
 import MyPagesTabs from '@components/myPage/MyPagesTabs';
-import { useEffect } from 'react';
 import useInterviewSettings from '@hooks/atoms/useInterviewSettings';
+import { useEffect } from 'react';
 
 const MyPage: React.FC = () => {
   const { resetAllSettings } = useInterviewSettings();
 
   useEffect(() => {
+    resetAllSettings();
     return () => {
       resetAllSettings();
     };
-  });
+  }, []);
 
   return (
     <MyPageLayout>
