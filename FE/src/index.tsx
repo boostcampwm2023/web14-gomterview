f258c2ff9d59f6fb4bd197b767af470461718f64
YoonHaeMin <yunhatmi@gachon.ac.kr>
2023-11-23 18:03:20 +0900

    feat: sentry 세팅 추가 (#102)

--- FE/src/index.tsx
+++ FE/src/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 import ReactGA from 'react-ga4';
 import { worker } from '@/mocks/browser';
 import App from '@/App';
+import * as Sentry from '@sentry/react';
 
 async function deferRender() {
   if (process.env.NODE_ENV !== 'development') {
@@ -14,6 +15,25 @@ async function deferRender() {
 if (process.env.REACT_APP_GTAG_ID) {
   ReactGA.initialize(process.env.REACT_APP_GTAG_ID);
 }
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: [
+        'localhost',
+        process.env.REACT_APP_SENTRY_BASE_URL!,
+      ],
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 deferRender()
   .then(() => {
