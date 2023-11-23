fa0b08e2ce6f230c2c8634273f73ce5dcc62876b
Sumin <robolindasoo@gmail.com>
2023-11-23 14:34:58 +0900

    feat: 마이페이지에서 나갈 때 전역 상태 모두 삭제

--- FE/src/page/myPage/index.tsx
+++ FE/src/page/myPage/index.tsx
@@ -1,8 +1,18 @@
 import MyPageLayout from '@/components/myPage/MyPageLayout';
 import Profile from '@/components/myPage/Profile';
 import MyPagesTabs from '@components/myPage/MyPagesTabs';
+import { useEffect } from 'react';
+import useInterviewSettings from '@hooks/atoms/useInterviewSettings';
 
 const MyPage: React.FC = () => {
+  const { resetAllSettings } = useInterviewSettings();
+
+  useEffect(() => {
+    return () => {
+      resetAllSettings();
+    };
+  });
+
   return (
     <MyPageLayout>
       <Profile />
