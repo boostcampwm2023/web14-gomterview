309a4adc6f9f793be16e2748ec9c97f73f99aed2
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2023-11-23 13:15:36 +0900

    feat: atom 상태를 전부 관리하는 hook 생성

---
+++ FE/src/hooks/atoms/useSettings.ts
@@ -0,0 +1,29 @@
+import { useRecoilState } from 'recoil';
+import {
+  questionSetting,
+  videoSetting,
+  recordSetting,
+} from '@atoms/interviewSetting';
+
+function useSettings() {
+  const [{ isSuccess: questionSuccess }, setQuestion] =
+    useRecoilState(questionSetting);
+  const [{ isSuccess: videoSuccess }, setVideo] = useRecoilState(videoSetting);
+  const [{ isSuccess: recordSuccess }, setRecord] =
+    useRecoilState(recordSetting);
+
+  const isAllSuccess = questionSuccess && videoSuccess && recordSuccess;
+
+  const resetAllSettings = () => {
+    setQuestion({ isSuccess: false, selectedData: [] });
+    setVideo({ isSuccess: false });
+    setRecord({ isSuccess: false, method: undefined });
+  };
+
+  return {
+    isAllSuccess,
+    resetAllSettings,
+  };
+}
+
+export default useSettings;
