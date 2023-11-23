aa85f661bceee3d3f235221afcb25df5ef17aa88
adultlee <seoungin1228@gmail.com>
2023-11-23 19:59:46 +0900

    fix: Question Answer 모달  scroll 변경

--- FE/src/components/common/QuestionSelectionBox/AnswerSelectionModal/AnswerSelectionModal.tsx
+++ FE/src/components/common/QuestionSelectionBox/AnswerSelectionModal/AnswerSelectionModal.tsx
@@ -37,7 +37,10 @@ const AnswerSelectionModal: React.FC<AnswerSelectionModalProps> = ({
       <Modal.content>
         <div
           css={css`
-            max-width: 60rem;
+            max-width: 40rem;
+            width: 100%;
+            max-height: 80vh;
+            overflow-y: auto;
           `}
         >
           <AnswerForm
