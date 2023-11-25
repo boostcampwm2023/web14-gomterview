173f539b5ef69bd4914ffff49ca2d52db80a5f62
quiet-honey <skap0329@naver.com>
2023-11-25 13:10:35 +0900

    docs: default API(/, 더미데이터 만드는 API)와 개발자용 토큰 반환하는 API Swagger 문서에서 제외

--- BE/src/app.controller.ts
+++ BE/src/app.controller.ts
@@ -1,16 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiExcludeEndpoint()
   async getHello() {
     return this.appService.getHello();
   }
 
   @Get('/initializeDummy')
+  @ApiExcludeEndpoint()
   async saveDummy() {
     await this.appService.saveDummy();
   }
