6142a7897c27ea0742a9a9fa79166b1cede6d5c9
Sumin <robolindasoo@gmail.com>
2023-11-26 11:41:06 +0900

    feat: 개발 환경에서 쿠키가 자동으로 발급되도록 변경

--- FE/src/components/landingPage/GoogleLoginButton.tsx
+++ FE/src/components/landingPage/GoogleLoginButton.tsx
@@ -5,6 +5,7 @@ import Icon from '@foundation/Icon/Icon';
 import { HTMLElementTypes } from '@/types/utils';
 import useUserInfo from '@hooks/useUserInfo';
 import { API, BASE_URL } from '@constants/api';
+import cookieGenerator from '@/dev/cookieGenerator';
 import { useNavigate } from 'react-router-dom';
 import { PATH } from '@constants/path';
 
@@ -14,9 +15,9 @@ const GoogleLoginButton: React.FC<GoogleLoginButtonProps> = ({ ...args }) => {
   const userInfo = useUserInfo();
   const navigate = useNavigate();
 
-  const handleGoogleLogin = () => {
-    if (userInfo) {
-      navigate(PATH.MYPAGE);
+  const handleGoogleLogin = async () => {
+    if (process.env.NODE_ENV === 'development') {
+      await cookieGenerator().then((_) => navigate(PATH.MYPAGE));
       return;
     }
 
@@ -24,30 +25,32 @@ const GoogleLoginButton: React.FC<GoogleLoginButtonProps> = ({ ...args }) => {
   };
 
   return (
-    <button
-      onClick={handleGoogleLogin}
-      css={css`
-        display: flex;
-        align-items: center;
-        column-gap: 0.75rem;
-        padding: 1.5rem 3rem;
-        border-radius: 3.125rem;
-        border: 0.0625rem solid ${theme.colors.border.default};
-        transition: transform 0.15s ease-in-out;
-        background-color: ${theme.colors.surface.default};
-        z-index: 1000;
+    !userInfo && (
+      <button
+        onClick={() => handleGoogleLogin()}
+        css={css`
+          display: flex;
+          align-items: center;
+          column-gap: 0.75rem;
+          padding: 1.5rem 3rem;
+          border-radius: 3.125rem;
+          border: 0.0625rem solid ${theme.colors.border.default};
+          transition: transform 0.15s ease-in-out;
+          background-color: ${theme.colors.surface.default};
+          z-index: 1000;
 
-        &:hover {
-          transform: translateY(-0.25rem);
-        }
-      `}
-      {...args}
-    >
-      {!userInfo && <Icon id="google-logo" width="1.25rem" height="1.25rem" />}
-      <Typography variant="title3" color={theme.colors.text.default}>
-        {userInfo ? '마이페이지로 이동하기' : 'Google로 시작하기'}
-      </Typography>
-    </button>
+          &:hover {
+            transform: translateY(-0.25rem);
+          }
+        `}
+        {...args}
+      >
+        <Icon id="google-logo" width="1.25rem" height="1.25rem" />
+        <Typography variant="title3" color={theme.colors.text.default}>
+          Google로 시작하기
+        </Typography>
+      </button>
+    )
   );
 };
 
