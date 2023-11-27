e08bd8baac812beb7293dcaffa3d6c490effb678
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2023-11-27 16:31:47 +0900

    refactor: eslint 설정 변경

--- FE/src/components/common/QuestionSelectionBox/QuestionSelectionBox.tsx
+++ FE/src/components/common/QuestionSelectionBox/QuestionSelectionBox.tsx
@@ -14,8 +14,6 @@ import { QuestionAnswerSelectionModal } from '@atoms/modal';
 
 const QuestionSelectionBox = () => {
   const [selectedTabIndex, setSelectedTabIndex] = useState('0');
-  //TODO: 현재 선택된 값은 임의 값임 Tabs를 변경한다면 가장 먼저 수정해야 할 사안.
-  // HOW: 해당 state는 몇번째 Tab을 클릭했는지 저장하는 state이다. 따라서 Index값을 가지고 있고 이를 바탕으로 questionAPI 데이터를 가져와야 한다.
 
   const { data: categoriesAPIData } = useCategoryQuery();
 
