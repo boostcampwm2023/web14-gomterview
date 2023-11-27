7fa2c5d5ab53a59b3d99817d1979f99ae61e7faa
JangAJang <janghee5395@gmail.com>
2023-11-27 18:11:59 +0900

    feat: 단위테스트를 통과하는 비즈니스 로직 구현

--- BE/src/workbook/service/workbook.service.ts
+++ BE/src/workbook/service/workbook.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { WorkbookRepository } from '../repository/workbook.repository';
+import { CreateWorkbookRequest } from '../dto/createWorkbookRequest';
+import { CategoryRepository } from '../../category/repository/category.repository';
+import { validateCategory } from '../../category/util/category.util';
+import { Workbook } from '../entity/workbook';
+import { Member } from '../../member/entity/member';
+import { validateManipulatedToken } from '../../util/token.util';
 
 @Injectable()
-export class WorkbookService {}
+export class WorkbookService {
+  constructor(
+    private workbookRepository: WorkbookRepository,
+    private categoryRepository: CategoryRepository,
+  ) {}
+
+  async createWorkbook(
+    createWorkbookRequest: CreateWorkbookRequest,
+    member: Member,
+  ) {
+    const category = await this.categoryRepository.findByCategoryId(
+      createWorkbookRequest.categoryId,
+    );
+    validateManipulatedToken(member);
+    validateCategory(category);
+
+    return await this.workbookRepository.save(
+      Workbook.of(
+        createWorkbookRequest.name,
+        createWorkbookRequest.content,
+        category,
+        member,
+      ),
+    );
+  }
+}
