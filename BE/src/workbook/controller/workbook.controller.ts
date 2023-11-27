1a9e260767ebc332015dda4c58a19c1cd9b3c539
JangAJang <janghee5395@gmail.com>
2023-11-27 17:32:09 +0900

    feat: 컨트롤러와 서비스 레이어 추가

---
+++ BE/src/workbook/controller/workbook.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('workbook')
+export class WorkbookController {}
--- BE/src/workbook/workbook.module.ts
+++ BE/src/workbook/workbook.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Workbook } from './entity/workbook';
 import { WorkbookRepository } from './repository/workbook.repository';
+import { WorkbookService } from './service/workbook.service';
+import { WorkbookController } from './controller/workbook.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Workbook])],
-  providers: [WorkbookRepository],
+  providers: [WorkbookRepository, WorkbookService],
+  controllers: [WorkbookController],
 })
 export class WorkbookModule {}
