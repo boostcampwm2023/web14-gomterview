01ebc0eb719ae91bb37bb1774e0a2f4d372df6de
JangAJang <janghee5395@gmail.com>
2023-11-28 11:26:45 +0900

    test: 통합테스트 구현

--- BE/src/workbook/service/workbook.service.spec.ts
+++ BE/src/workbook/service/workbook.service.spec.ts
@@ -10,8 +10,19 @@ import {
 import { memberFixture } from '../../member/fixture/member.fixture';
 import { CategoryNotFoundException } from '../../category/exception/category.exception';
 import { ManipulatedTokenNotFiltered } from '../../token/exception/token.exception';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { MemberRepository } from '../../member/repository/member.repository';
+import { AuthModule } from '../../auth/auth.module';
+import { WorkbookModule } from '../workbook.module';
+import { CategoryModule } from '../../category/category.module';
+import { Member } from '../../member/entity/member';
+import { Workbook } from '../entity/workbook';
+import { createIntegrationTestModule } from '../../util/test.util';
+import * as cookieParser from 'cookie-parser';
+import { Category } from '../../category/entity/category';
+import { CreateWorkbookRequest } from '../dto/createWorkbookRequest';
 
-describe('WorkbookService', () => {
+describe('WorkbookService 단위테스트', () => {
   let service: WorkbookService;
   const mockCategoryRepository = {
     findByCategoryId: jest.fn(),
@@ -80,5 +91,54 @@ describe('WorkbookService', () => {
         service.createWorkbook(createWorkbookRequestFixture, null),
       ).rejects.toThrow(new ManipulatedTokenNotFiltered());
     });
+  });
+});
+
+describe('WorkbookService 통합테스트', () => {
+  let app: INestApplication;
+  let categoryRepository: CategoryRepository;
+  let memberRepository: MemberRepository;
+  let workbookService: WorkbookService;
+
+  beforeAll(async () => {
+    const modules = [AuthModule, WorkbookModule, CategoryModule];
+    const entities = [Member, Workbook, Category];
+
+    const moduleFixture: TestingModule = await createIntegrationTestModule(
+      modules,
+      entities,
+    );
+
+    app = moduleFixture.createNestApplication();
+    app.use(cookieParser());
+    app.useGlobalPipes(new ValidationPipe());
+    await app.init();
+
+    memberRepository = moduleFixture.get<MemberRepository>(MemberRepository);
+    categoryRepository =
+      moduleFixture.get<CategoryRepository>(CategoryRepository);
+    workbookService = moduleFixture.get<WorkbookService>(WorkbookService);
+  });
+
+  it('회원의 문제집을 생성한다.', async () => {
+    //given
+    const member = await memberRepository.save(memberFixture);
+    const category = await categoryRepository.save(categoryFixtureWithId);
+
+    //when
+    const createWorkbookRequest = new CreateWorkbookRequest(
+      'test name',
+      'test content',
+      category.id,
+    );
+    const workbook = await workbookService.createWorkbook(
+      createWorkbookRequest,
+      member,
+    );
+
+    //then
+    expect(workbook.name).toEqual('test name');
+    expect(workbook.content).toEqual('test content');
+    expect(workbook.member.id).toEqual(member.id);
   });
 });
