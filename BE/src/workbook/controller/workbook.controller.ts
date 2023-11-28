37d85a7647766b44f74afba1f150ad32a4d68b4e
JangAJang <janghee5395@gmail.com>
2023-11-28 12:16:54 +0900

    feat: 컨트롤러 로직 구현

--- BE/src/workbook/controller/workbook.controller.ts
+++ BE/src/workbook/controller/workbook.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { WorkbookService } from '../service/workbook.service';
+import { AuthGuard } from '@nestjs/passport';
+import {
+  ApiBody,
+  ApiCookieAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { createApiResponseOption } from '../../util/swagger.util';
+import { Request } from 'express';
+import { CreateWorkbookRequest } from '../dto/createWorkbookRequest';
+import { Member } from '../../member/entity/member';
+import { WorkbookIdResponse } from '../dto/workbookIdResponse';
 
-@Controller('workbook')
-export class WorkbookController {}
+@ApiTags('workbook')
+@Controller('/api/workbook')
+export class WorkbookController {
+  constructor(private workbookService: WorkbookService) {}
+
+  @Post()
+  @UseGuards(AuthGuard('jwt'))
+  @ApiCookieAuth()
+  @ApiBody({ type: CreateWorkbookRequest })
+  @ApiOperation({
+    summary: '새로운 문제집 추가 추가',
+  })
+  @ApiResponse(
+    createApiResponseOption(201, '문제집 생성 완료', WorkbookIdResponse),
+  )
+  async createAnswer(
+    @Body() createWorkbookRequest: CreateWorkbookRequest,
+    @Req() req: Request,
+  ) {
+    const workbook = await this.workbookService.createWorkbook(
+      createWorkbookRequest,
+      req.user as Member,
+    );
+
+    return WorkbookIdResponse.of(workbook);
+  }
+}
