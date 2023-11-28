caecd1f0d76e5729514ec8fc59204896d09de891
adultlee <seoungin1228@gmail.com>
2023-11-28 13:50:38 +0900

    feat: Menu에서 사용될 Item에 대한 정의

---
+++ FE/src/components/layout/Header/MenuItem.tsx
@@ -0,0 +1,39 @@
+import { css } from '@emotion/react';
+import { Typography } from '@foundation/index';
+import { theme } from '@styles/theme';
+import { Link } from 'react-router-dom';
+
+type MenuItemProps = {
+  path: string;
+  text: string;
+};
+
+const MenuItem: React.FC<MenuItemProps> = ({ path, text }) => {
+  return (
+    <div
+      css={css`
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        cursor: pointer;
+        padding: 0.5rem;
+        :hover {
+          background-color: ${theme.colors.surface.weak};
+        }
+      `}
+    >
+      <Link
+        to={path}
+        css={css`
+          text-decoration: none;
+        `}
+      >
+        <Typography variant="body1" color={theme.colors.text.sub}>
+          {text}
+        </Typography>
+      </Link>
+    </div>
+  );
+};
+
+export default MenuItem;
