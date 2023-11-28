4a6568d87e701bcae2249968b4b4ffe92a1b5a63
Sumin <robolindasoo@gmail.com>
2023-11-28 16:11:02 +0900

    feat: 함수의 파라미터의 타입을 추론하기 위한 유틸 타입 생성

--- FE/src/types/utils.ts
+++ FE/src/types/utils.ts
@@ -4,3 +4,7 @@ export type HTMLElementTypes<T> = React.ClassAttributes<T> &
   React.HTMLAttributes<T> & {
     css?: Interpolation<Theme>;
   };
+
+export type FunctionParamsType<T> = T extends (...args: infer P) => unknown
+  ? P
+  : never;
