9c796c2a4b339356cb215fd94ea19d422454b1a6
adultlee <seoungin1228@gmail.com>
2023-11-28 14:33:52 +0900

    feat: SideMenu 적용

---
+++ FE/src/components/layout/Header/SideMenu.tsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import { css, keyframes } from '@emotion/react';
+import { theme } from '@styles/theme';
+import { Typography } from '@foundation/index';
+
+const dropDown = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+type SideMenuProps = {
+  children: React.ReactNode;
+};
+
+const SideMenu: React.FC<SideMenuProps> = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div>
+      <div
+        onClick={toggleMenu}
+        css={css`
+          display: flex;
+          justify-content: flex-start;
+          align-items: center;
+          cursor: pointer;
+          padding: 0.5rem;
+          :hover {
+            background-color: ${theme.colors.surface.weak};
+          }
+        `}
+      >
+        {!isOpen && (
+          <Typography variant="body1" color={theme.colors.text.sub}>
+            메뉴
+          </Typography>
+        )}
+      </div>
+      {isOpen && (
+        <>
+          <div
+            css={css`
+              position: absolute;
+              top: 40px;
+              right: 0;
+              background-color: ${theme.colors.surface.weak};
+              border: 1px solid #ddd;
+              border-radius: 4px;
+              padding: 8px;
+              box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+              animation: ${dropDown} 300ms ease-out;
+            `}
+          >
+            {children}
+          </div>
+          <div
+            onClick={closeMenu}
+            css={css`
+              position: fixed;
+              top: 0;
+              left: 0;
+              width: 100%;
+              height: 100%;
+              background-color: rgba(0, 0, 0, 0.5);
+              z-index: -1;
+            `}
+          />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default SideMenu;
