75d6ed4c3686c0b6505cc998136ae5982bca3104
adultlee <seoungin1228@gmail.com>
2023-11-28 14:00:08 +0900

    feat: MenuList로 변경

--- FE/src/components/layout/Header/Menu.tsx
+++ FE/src/components/layout/Header/Menu.tsx
@@ -1,6 +1,6 @@
-import { PATH } from '@constants/path';
 import { css } from '@emotion/react';
-import MenuItem from './MenuItem';
+import MenuList from './MenuList';
+
 const Menu = () => {
   return (
     <div
@@ -9,9 +9,7 @@ const Menu = () => {
         gap: 1rem;
       `}
     >
-      <MenuItem path={PATH.ROOT} text="면접 Set 보러가기" />
-      <MenuItem path={PATH.INTERVIEW_SETTING} text="면접 문제 풀러가기" />
-      <MenuItem path={PATH.MYPAGE} text="마이페이지" />
+      <MenuList />
     </div>
   );
 };
