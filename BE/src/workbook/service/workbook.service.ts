fec11059be56f2fd3e8952a876cd80a3a8fe791b
JangAJang <janghee5395@gmail.com>
2023-11-28 16:15:47 +0900

    feat: 단위테스트를 성공하는 비즈니스 로직 구현

--- BE/src/workbook/service/workbook.service.ts
+++ BE/src/workbook/service/workbook.service.ts
@@ -6,6 +6,9 @@ import { validateCategory } from '../../category/util/category.util';
 import { Workbook } from '../entity/workbook';
 import { Member } from '../../member/entity/member';
 import { validateManipulatedToken } from '../../util/token.util';
+import { WorkbookResponse } from '../dto/workbookResponse';
+import { isEmpty } from 'class-validator';
+import { validateWorkbook } from '../util/workbook.util';
 
 @Injectable()
 export class WorkbookService {
@@ -32,5 +35,46 @@ export class WorkbookService {
         member,
       ),
     );
+  }
+
+  async findWorkbooks(categoryId: number) {
+    if (isEmpty(categoryId)) {
+      return await this.findAllWorkbook();
+    }
+
+    return this.findAllByCategory(categoryId);
+  }
+
+  private async findAllWorkbook() {
+    const workbooks = await this.workbookRepository.findAll();
+    return workbooks.map(WorkbookResponse.of);
+  }
+
+  private async findAllByCategory(categoryId: number) {
+    const category = await this.categoryRepository.findByCategoryId(categoryId);
+    validateCategory(category);
+
+    const workbooks =
+      await this.workbookRepository.findAllByCategoryId(categoryId);
+    return workbooks.map(WorkbookResponse.of);
+  }
+
+  async findWorkbookTitles(member: Member) {
+    if (isEmpty(member)) {
+      return (await this.workbookRepository.findTop5Workbooks()).map(
+        WorkbookResponse.of,
+      );
+    }
+
+    return (await this.workbookRepository.findMembersWorkbooks(member.id)).map(
+      WorkbookResponse.of,
+    );
+  }
+
+  async findSingleWorkbook(workbookId: number) {
+    const workbook = await this.workbookRepository.findById(workbookId);
+    validateWorkbook(workbook);
+
+    return WorkbookResponse.of(workbook);
   }
 }
