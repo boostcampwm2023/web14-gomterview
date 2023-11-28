46fc73de8612be97554fa85c5a5f3a66cd39aefb
Sumin <robolindasoo@gmail.com>
2023-11-28 11:54:10 +0900

    feat: 질문 복사 관련 dto 정의

--- FE/src/types/question.ts
+++ FE/src/types/question.ts
@@ -4,3 +4,20 @@ export type Question = {
   answerId: number;
   answerContent: string;
 };
+
+/**
+ * POST question/copy
+ * 문제집에서 질문을 새로운 문제집으로 복사할 때 요청 객체 타입입니다.
+ */
+export type QuestionCopyReqDto = {
+  workbookId: number;
+  questionIds: number[];
+};
+
+/**
+ * POST question/copy
+ * 문제집에서 질문을 새로운 문제집으로 복사할 때 응답 객체 타입입니다.
+ */
+export type QuestionCopyResDto = {
+  workbookId: number;
+};
