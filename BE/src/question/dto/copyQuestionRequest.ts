566b2477d8775004247d86d549cf99ea9e232cee
JangAJang <janghee5395@gmail.com>
2023-11-29 17:46:14 +0900

    feat: 질문 복제 요청을 받는 dto 구현

---
+++ BE/src/question/dto/copyQuestionRequest.ts
@@ -0,0 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { createPropertyOption } from '../../util/swagger.util';
+import { IsNotEmpty, IsNumber } from 'class-validator';
+
+export class CopyQuestionRequest {
+  @ApiProperty(createPropertyOption('1', '문제집 id', Number))
+  @IsNotEmpty()
+  @IsNumber()
+  workbookId: number;
+
+  @ApiProperty(
+    createPropertyOption([1, 2, 3, 4, 5], '복사할 질문들의 id', Number),
+  )
+  @IsNotEmpty()
+  @IsNumber()
+  questionIds: number[];
+
+  constructor(workbookId: number, questionIds: number[]) {
+    this.workbookId = workbookId;
+    this.questionIds = questionIds;
+  }
+}
