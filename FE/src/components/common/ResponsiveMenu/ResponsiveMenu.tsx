062853199421d2a45969533f810575ea8ae104e5
adultlee <seoungin1228@gmail.com>
2023-11-29 21:35:24 +0900

    fix: 범용적인 사용을 위해 Box -> div로 변경 후 스타일 추가
    Tab 컴포넌트를 보고 참고 했습니다!

--- FE/src/components/common/ResponsiveMenu/ResponsiveMenu.tsx
+++ FE/src/components/common/ResponsiveMenu/ResponsiveMenu.tsx
@@ -1,4 +1,3 @@
-import { Box } from '@foundation/index';
 import { css } from '@emotion/react';
 import { HTMLElementTypes } from '@/types/utils';
 import useBreakpoint from '@hooks/useBreakPoint';
@@ -7,11 +6,16 @@ import useThrottleScroll from '@hooks/useThrottleScroll';
 type ResponsiveMenuType = HTMLElementTypes<HTMLDivElement> & {
   location?: 'left' | 'right';
   top?: number;
+  inlineStyle?: string;
+  sideStyle?: string;
 };
+
 const ResponsiveMenu: React.FC<ResponsiveMenuType> = ({
   children,
   location = 'left',
   top = 100,
+  inlineStyle = ``,
+  sideStyle = ``,
   ...arg
 }) => {
   const isDeviceBreakpoint = useBreakpoint();
@@ -23,16 +27,18 @@ const ResponsiveMenu: React.FC<ResponsiveMenuType> = ({
     transition: transform 0.3s linear;
     width: auto;
     ${location === 'left' ? 'left: -120px;' : 'right: -120px;'}
+    ${sideStyle}
   `;
 
   const InlineCSS = css`
     position: static;
     transform: none;
     width: 100%;
+    ${inlineStyle}
   `;
 
   return (
-    <Box
+    <div
       css={css`
         height: auto;
         ${isDeviceBreakpoint('laptop') ? InlineCSS : SideCSS}
@@ -40,7 +46,7 @@ const ResponsiveMenu: React.FC<ResponsiveMenuType> = ({
       {...arg}
     >
       {children}
-    </Box>
+    </div>
   );
 };
 
