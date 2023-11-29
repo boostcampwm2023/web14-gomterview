c76082863249894cd27bab92a0ef3cd3f899ade1
Sumin <robolindasoo@gmail.com>
2023-11-29 16:36:42 +0900

    docs: query 훅에 jsdocs로 설명 추가

--- FE/src/hooks/apis/queries/useWorkbookListQuery.ts
+++ FE/src/hooks/apis/queries/useWorkbookListQuery.ts
@@ -2,6 +2,13 @@ import { useQuery } from '@tanstack/react-query';
 import { QUERY_KEY } from '@constants/queryKey';
 import { getWorkbookByCategory } from '@/apis/workbook';
 
+/**
+ * GET /workbook?category=${categoryId}
+ *
+ * categoryId로 해당 카테고리의 모든 문제집을 조회하는 api입니다.
+ *
+ * 문제집 리스트 페이지에서 사용됩니다.
+ */
 const useWorkbookListQuery = (categoryId: number) => {
   return useQuery({
     queryKey: QUERY_KEY.WORKBOOK,
--- FE/src/hooks/apis/queries/useWorkbookTitleListQuery.ts
+++ FE/src/hooks/apis/queries/useWorkbookTitleListQuery.ts
@@ -2,6 +2,13 @@ import { useQuery } from '@tanstack/react-query';
 import { QUERY_KEY } from '@constants/queryKey';
 import { getWorkbookTitle } from '@/apis/workbook';
 
+/**
+ * GET /workbook/title
+ *
+ * 나의 문제집 제목을 조회하는 api입니다.
+ *
+ * 비회원은 탑5 문제집이 조회되고, 회원은 나의 문제집이 조회됩니다.
+ */
 const useWorkbookTitleListQuery = () => {
   return useQuery({
     queryKey: QUERY_KEY.WORKBOOK,
