7c00060382364c197261be5cd37e97a789106d5a
JangAJang <janghee5395@gmail.com>
2023-12-02 18:01:20 +0900

    test: 테스트 수정

--- BE/src/question/repository/question.repository.ts
+++ BE/src/question/repository/question.repository.ts
@@ -31,6 +31,16 @@ export class QuestionRepository {
       .getMany();
   }
 
+  async findAllByIds(ids: number[]) {
+    return await this.repository
+      .createQueryBuilder('Question')
+      .leftJoinAndSelect('Question.workbook', 'workbook')
+      .leftJoinAndSelect('Question.origin', 'origin')
+      .leftJoinAndSelect('Question.defaultAnswer', 'defaultAnswer')
+      .where('Question.id IN (:...ids)', { ids })
+      .getMany();
+  }
+
   async findById(questionId: number) {
     return await this.repository.findOneBy({ id: questionId });
   }
