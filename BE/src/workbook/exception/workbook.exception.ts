ee6646696565d9ef1bab22195b7ba78389dacb94
JangAJang <janghee5395@gmail.com>
2023-12-04 18:10:34 +0900

    feat: 에러메시지를 상태코드/메시지가 아닌, 에러메시지/커스텀에러코드의 구조로 반환하게 기능 구현

--- BE/src/workbook/exception/workbook.exception.ts
+++ BE/src/workbook/exception/workbook.exception.ts
@@ -2,13 +2,13 @@ import { HttpException } from '@nestjs/common';
 
 class WorkbookNotFoundException extends HttpException {
   constructor() {
-    super('문제집을 찾을 수 없습니다.', 404);
+    super({ message: '문제집을 찾을 수 없습니다.', errorCode: 'W01' }, 404);
   }
 }
 
 class WorkbookForbiddenException extends HttpException {
   constructor() {
-    super('문제집에 대한 권한이 없습니다.', 403);
+    super({ message: '문제집에 대한 권한이 없습니다.', errorCode: 'W02' }, 403);
   }
 }
 
