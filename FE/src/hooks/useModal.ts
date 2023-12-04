76e43be58bbcab98e486a835084872d66d36f51d
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2023-12-04 21:56:15 +0900

    fix: 모달 여러개가 띄워졌을 때 배경 스크롤 문제 해결
    - 기존: 모달 하나만 닫혀도 배경 스크롤이 가능
    - 수정: 모달이 모두 닫혀야 배경 스크롤 가능

--- FE/src/hooks/useModal.ts
+++ FE/src/hooks/useModal.ts
@@ -1,32 +1,29 @@
 import { modalState } from '@atoms/modal';
-import { useCallback, useEffect, useId, useMemo, useState } from 'react';
+import { useCallback, useId, useMemo, useState } from 'react';
 import { useRecoilState } from 'recoil';
 
+const isArrEmpty = (arr: unknown[]) => arr.length === 0;
+
 const useModal = (component: React.FC) => {
-  const [, setModal] = useRecoilState(modalState);
+  const [modalElements, setModal] = useRecoilState(modalState);
   const [isOpen, setIsOpen] = useState(false);
 
   const id = useId();
 
   const openModal = useCallback(() => {
     setIsOpen(true);
+    setModal((pre) => [...pre, { id: id, element: modalComponent }]);
     document.body.style.overflow = 'hidden';
   }, []);
 
   const closeModal = useCallback(() => {
     setIsOpen(false);
-    document.body.style.overflow = 'unset';
+    setModal((pre) => pre.filter((c) => c.id !== id));
+
+    if (isArrEmpty(modalElements)) document.body.style.overflow = 'unset';
   }, []);
 
   const modalComponent = useMemo(() => component, []);
-
-  useEffect(() => {
-    if (isOpen) {
-      setModal((pre) => [...pre, { id: id, element: modalComponent }]);
-    } else {
-      setModal((pre) => pre.filter((c) => c.id !== id));
-    }
-  }, [id, isOpen, modalComponent, setModal]);
 
   return { isOpen, openModal, closeModal };
 };
