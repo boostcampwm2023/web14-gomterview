a6ae549331b96e0bc882d344cc337818f7e63cf5
adultlee <seoungin1228@gmail.com>
2023-12-05 16:26:50 +0900

    feat: microphone의 volume을 통제 할 수 있는 util 함수 구현

--- FE/src/utils/media.ts
+++ FE/src/utils/media.ts
@@ -35,3 +35,48 @@ export const getSupportedMimeTypes = () => {
   ];
   return types.filter((type) => MediaRecorder.isTypeSupported(type));
 };
+
+export const createMicrophoneVolumeMonitor = (
+  stream: MediaStream,
+  volumeCallback: React.Dispatch<React.SetStateAction<number>>
+) => {
+  const audioContext = new AudioContext();
+  const sourceNode = audioContext.createMediaStreamSource(stream);
+  const analyser = audioContext.createAnalyser();
+  analyser.fftSize = 2048;
+
+  sourceNode.connect(analyser);
+  const pcmData = new Float32Array(analyser.fftSize);
+
+  let intervalId: NodeJS.Timer | number;
+
+  const startMonitoring = () => {
+    intervalId = setInterval(() => {
+      analyser.getFloatTimeDomainData(pcmData);
+
+      let sumSquares = 0;
+      for (let i = 0; i < pcmData.length; i++) {
+        sumSquares += pcmData[i] * pcmData[i];
+      }
+      const rms = Math.sqrt(sumSquares / pcmData.length);
+
+      const weight = 7;
+      const amplifiedVolume = rms * weight;
+
+      const normalizedVolume = Math.min(Math.round(amplifiedVolume * 100), 100);
+
+      volumeCallback(normalizedVolume);
+    }, 150);
+  };
+
+  const stopMonitoring = () => {
+    if (intervalId) {
+      clearInterval(intervalId as number);
+    }
+    sourceNode.disconnect();
+    analyser.disconnect();
+    void audioContext.close();
+  };
+
+  return { startMonitoring, stopMonitoring };
+};
