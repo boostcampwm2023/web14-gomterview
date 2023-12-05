e14a23e96ffbf4edcc3214ea9b27759ac3f240f6
Sumin <robolindasoo@gmail.com>
2023-12-05 17:46:58 +0900

    feat: 비디오 정보 불러오기 오류시 에러 페이지로 이동

--- FE/src/page/interviewVideoPage/index.tsx
+++ FE/src/page/interviewVideoPage/index.tsx
@@ -3,27 +3,41 @@ import {
   InterviewVideoPageLayout,
   PrivateVideoPlayer,
 } from '@components/interviewVideoPage';
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 import { CenterLayout } from '@components/layout';
 import { PATH } from '@constants/path';
+import { ErrorBoundary } from 'react-error-boundary';
+import { isAxiosError } from 'axios';
 
 const InterviewVideoPage: React.FC = () => {
   const { videoId } = useParams();
+  const [errorInfo, setErrorInfo] = useState<Partial<Response>>();
 
   if (!videoId) return <Navigate to={PATH.ROOT} />;
 
   return (
     <InterviewVideoPageLayout>
-      <Suspense
-        fallback={
-          <CenterLayout>
-            <LoadingBounce />
-          </CenterLayout>
-        }
+      <ErrorBoundary
+        onError={(err) => {
+          if (!isAxiosError(err)) return;
+          setErrorInfo({
+            status: err?.response?.status,
+            statusText: err.response?.statusText,
+          });
+        }}
+        fallback={<Navigate to={PATH.NOT_FOUND} state={errorInfo} />}
       >
-        <PrivateVideoPlayer videoId={videoId} />
-      </Suspense>
+        <Suspense
+          fallback={
+            <CenterLayout>
+              <LoadingBounce />
+            </CenterLayout>
+          }
+        >
+          <PrivateVideoPlayer videoId={videoId} />
+        </Suspense>
+      </ErrorBoundary>
       <StartButton />
     </InterviewVideoPageLayout>
   );
