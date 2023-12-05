5b8ef0b56613f58c077059c4a0dbc14a0b1fa724
JangAJang <janghee5395@gmail.com>
2023-12-05 18:39:05 +0900

    chore: 사용되지 않는 로직 제거

--- BE/src/category/entity/category.ts
+++ BE/src/category/entity/category.ts
@@ -1,6 +1,5 @@
 import { Column, Entity } from 'typeorm';
 import { DefaultEntity } from '../../app.entity';
-import { CreateCategoryRequest } from '../dto/createCategoryRequest';
 
 @Entity({ name: 'Category' })
 export class Category extends DefaultEntity {
@@ -10,13 +9,5 @@ export class Category extends DefaultEntity {
   constructor(id: number, name: string, createdAt: Date) {
     super(id, createdAt);
     this.name = name;
-  }
-
-  static from(inputObj: CreateCategoryRequest | Category) {
-    return new Category(null, inputObj.name, new Date());
-  }
-
-  static of(name: string) {
-    return new Category(null, name, new Date());
   }
 }
