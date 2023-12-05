9dcc3b2767e654bddcc8949e2dd3debf92e3ee5f
adultlee <seoungin1228@gmail.com>
2023-12-06 01:27:51 +0900

    feat: common에 Mirror 컴포넌트 추가

---
+++ FE/src/components/common/Mirror/Mirror.tsx
@@ -0,0 +1,74 @@
+import { css } from '@emotion/react';
+import { Icon } from '@foundation/index';
+import { Tooltip } from '@foundation/index';
+import { theme } from '@styles/theme';
+import React, { RefObject } from 'react';
+
+type MirrorProps = {
+  mirrorVideoRef: RefObject<HTMLVideoElement>;
+  connectStatus: 'connect' | 'fail' | 'pending';
+  reloadMedia: () => void;
+  isSetting?: boolean;
+};
+
+const Mirror: React.FC<MirrorProps> = ({
+  mirrorVideoRef,
+  connectStatus,
+  reloadMedia,
+  isSetting,
+}) => {
+  return (
+    <>
+      <video
+        ref={mirrorVideoRef}
+        autoPlay
+        muted
+        css={css`
+          display: ${connectStatus === 'fail' ? 'none' : 'block'};
+          height: 100%;
+          width: ${isSetting && '100%'};
+          transform: scaleX(-1);
+          border-radius: 1rem;
+        `}
+      />
+      {connectStatus === 'fail' && (
+        <div
+          css={css`
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100%;
+            min-height: 300px;
+            width: 100%;
+            border-radius: 0.5rem;
+            background-color: ${theme.colors.surface.inner};
+          `}
+        >
+          <Tooltip title="카메라를 다시 연결합니다." position="bottom">
+            <div
+              onClick={reloadMedia}
+              css={css`
+                cursor: pointer;
+                width: 6.25rem;
+                height: 6.25rem;
+                border-radius: 50%;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                transition: all 0.5s ease;
+
+                &:hover {
+                  transform: scale(1.1);
+                }
+              `}
+            >
+              <Icon id="reload" width="80px" height="80px" />
+            </div>
+          </Tooltip>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Mirror;
