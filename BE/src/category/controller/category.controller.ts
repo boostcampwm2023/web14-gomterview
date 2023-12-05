c0f9e796570524c47d289e07026cc0d22e29f2ec
JangAJang <janghee5395@gmail.com>
2023-12-05 18:53:12 +0900

    chore: 카테고리 모듈에서 불필요하게 존재하는 코드 제거

--- BE/src/category/controller/category.controller.ts
+++ BE/src/category/controller/category.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CategoryService } from '../service/category.service';
 import { createApiResponseOption } from '../../util/swagger.util';
-import { CategoryListResponse } from '../dto/categoryListResponse';
+import { CategoryResponse } from '../dto/categoryResponse';
 
 @Controller('/api/category')
 @ApiTags('category')
@@ -14,11 +14,9 @@ export class CategoryController {
     summary: '전체 카테고리를 조회한다.',
   })
   @ApiResponse(
-    createApiResponseOption(
-      200,
-      '사용중인 카테고리 조회 추가',
-      CategoryListResponse,
-    ),
+    createApiResponseOption(200, '사용중인 카테고리 조회 추가', [
+      CategoryResponse,
+    ]),
   )
   async findCategories() {
     return await this.categoryService.findUsingCategories();
