e9beea2f17cd5fc40d9661afa926b222db0e35d5
JangAJang <janghee5395@gmail.com>
2023-12-05 10:58:18 +0900

    refactor: Answer 로직에 대한 예외에 에러코드 추가

--- BE/src/answer/exception/answer.exception.ts
+++ BE/src/answer/exception/answer.exception.ts
@@ -1,14 +1,17 @@
-import { HttpException } from '@nestjs/common';
+import {
+  HttpForbiddenException,
+  HttpNotFoundException,
+} from '../../util/exception.util';
 
-class AnswerNotFoundException extends HttpException {
+class AnswerNotFoundException extends HttpNotFoundException {
   constructor() {
-    super('해당 답변을 찾을 수 없습니다.', 404);
+    super('해당 답변을 찾을 수 없습니다.', 'A01');
   }
 }
 
-class AnswerForbiddenException extends HttpException {
+class AnswerForbiddenException extends HttpForbiddenException {
   constructor() {
-    super('답변에 대한 권한이 없습니다', 403);
+    super('답변에 대한 권한이 없습니다.', 'A02');
   }
 }
 
