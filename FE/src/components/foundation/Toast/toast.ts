020f7737a18147bc9b384b79f8aaa778380d4f33
Sumin <robolindasoo@gmail.com>
2023-12-07 03:14:04 +0900

    refactor: toast.type(text) 형식으로 출력할 수 있게 함수 변경

--- FE/src/components/foundation/Toast/toast.ts
+++ FE/src/components/foundation/Toast/toast.ts
@@ -1,10 +1,30 @@
-import { ToastEvent, ToastProps } from '@foundation/Toast/type';
+import { ToastEvent, ToastProps, ToastType } from '@foundation/Toast/type';
 import { eventManager } from '@foundation/Toast/eventManger';
 
+type ToastFunctionProps = Omit<ToastProps, 'toastId' | 'type'>;
+type ToastOptions = Omit<ToastProps, 'toastId' | 'type' | 'text'>;
 const generateUniqueId = () => {
   return Date.now().toString(36) + Math.random().toString(36).substring(4);
 };
-export const toast = (toastProps: Omit<ToastProps, 'toastId'>) => {
+
+const emitToast = (type: ToastType, toastProps: ToastFunctionProps) => {
   const id = generateUniqueId();
-  eventManager.emit(ToastEvent.Add, { ...toastProps, toastId: id });
+  eventManager.emit(ToastEvent.Add, {
+    ...toastProps,
+    toastId: id,
+    type,
+  });
+};
+
+export const toast = {
+  default: (text: string, toastOptions: ToastOptions) =>
+    emitToast('default', { text: text, ...toastOptions }),
+  info: (text: string, toastOptions: ToastOptions) =>
+    emitToast('info', { text: text, ...toastOptions }),
+  success: (text: string, toastOptions: ToastOptions) =>
+    emitToast('success', { text: text, ...toastOptions }),
+  warning: (text: string, toastOptions: ToastOptions) =>
+    emitToast('warning', { text: text, ...toastOptions }),
+  error: (text: string, toastOptions: ToastOptions) =>
+    emitToast('error', { text: text, ...toastOptions }),
 };
