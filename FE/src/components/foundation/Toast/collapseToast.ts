98537869e292d71c4279a5baad7461b17290c342
Sumin <robolindasoo@gmail.com>
2023-12-07 16:37:15 +0900

    design: 토스트 애니메이션 조정

--- FE/src/components/foundation/Toast/collapseToast.ts
+++ FE/src/components/foundation/Toast/collapseToast.ts
@@ -1,19 +1,16 @@
 export function collapseToast(
   node: HTMLDivElement,
   done: () => void,
-  duration = 500
+  duration = 1000
 ) {
   const { scrollHeight, style } = node;
 
   requestAnimationFrame(() => {
-    style.minHeight = 'initial';
     style.height = scrollHeight + 'px';
     style.transition = `all ${duration}ms`;
 
     requestAnimationFrame(() => {
       style.height = '0';
-      style.padding = '0';
-      style.margin = '0';
       setTimeout(done, duration);
     });
   });
