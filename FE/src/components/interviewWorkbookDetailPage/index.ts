cc5c2234f37d5288a5692a42d1e4275ca7bd7d93
adultlee <seoungin1228@gmail.com>
2023-12-07 20:23:13 +0900

    fix: export 하지 않았던 컴포넌트 반환

--- FE/src/components/interviewWorkbookDetailPage/index.ts
+++ FE/src/components/interviewWorkbookDetailPage/index.ts
@@ -1,2 +1,3 @@
 export { default as AddWorkbookListModal } from './AddWorkbookListModal';
 export { default as InterviewWorkbookDetailPageLayout } from './InterviewWorkbookDetailPageLayout';
+export { default as StartWithSelectedQuestionModal } from './StartWithSelectedQuestionModal';
