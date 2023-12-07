ac8f2ce81e95b82aa270359d28a6c4693ca0f144
adultlee <seoungin1228@gmail.com>
2023-12-08 00:06:07 +0900

    feat: 404 페이지 Header 추가

--- FE/src/components/errorPage/ErrorPageLayout.tsx
+++ FE/src/components/errorPage/ErrorPageLayout.tsx
@@ -2,21 +2,25 @@ import Layout from '../layout/Layout';
 import { PropsWithChildren } from 'react';
 import { css } from '@emotion/react';
 import { theme } from '@styles/theme';
+import { Header } from '@components/layout';
 
 const ErrorPageLayout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <Layout
-      full
-      direction="column"
-      css={css`
-        justify-content: center;
-        align-items: center;
-        row-gap: 1rem;
-        background: ${theme.gradient.linear.skyblue};
-      `}
-    >
-      {children}
-    </Layout>
+    <>
+      <Header />
+      <Layout
+        full
+        direction="column"
+        css={css`
+          justify-content: center;
+          align-items: center;
+          row-gap: 1rem;
+          background: ${theme.gradient.linear.skyblue};
+        `}
+      >
+        {children}
+      </Layout>
+    </>
   );
 };
 
