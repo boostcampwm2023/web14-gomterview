0f468c6b0d5f47992c7739b6e8d76aa5ae14367d
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2023-12-07 13:53:32 +0900

    refactor: error를 상위 errorPage로 위임

--- FE/src/routes/interviewVideoPublicLoader.ts
+++ FE/src/routes/interviewVideoPublicLoader.ts
@@ -2,7 +2,6 @@ import { QueryClient } from '@tanstack/react-query/build/modern/index';
 import { QUERY_KEY } from '@constants/queryKey';
 import { Params, redirect } from 'react-router-dom';
 import { getVideoByHash } from '@/apis/video';
-import axios from 'axios';
 import { PATH } from '@constants/path';
 
 const interviewVideoPublicLoader = async ({
@@ -13,20 +12,10 @@ const interviewVideoPublicLoader = async ({
   queryClient: QueryClient;
 }) => {
   const { videoHash = '' } = params;
-  await queryClient
-    .ensureQueryData({
-      queryKey: QUERY_KEY.VIDEO_HASH(videoHash),
-      queryFn: () => getVideoByHash(videoHash),
-    })
-    .catch((e) => {
-      if (axios.isAxiosError(e)) {
-        process.env.NODE_ENV === 'development' && console.error(e.toJSON());
-        throw new Response(null, {
-          status: e.response?.status ?? 500,
-          statusText: e.message,
-        });
-      }
-    });
+  await queryClient.ensureQueryData({
+    queryKey: QUERY_KEY.VIDEO_HASH(videoHash),
+    queryFn: () => getVideoByHash(videoHash),
+  });
   const queryState = queryClient.getQueryState(QUERY_KEY.VIDEO_HASH(videoHash));
   return queryState?.data ? null : redirect(PATH.NOT_FOUND);
 };
