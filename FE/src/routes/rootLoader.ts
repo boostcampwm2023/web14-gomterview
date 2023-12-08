b11b2a22537b310c62f1f8623e368eff509dc02f
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2023-12-08 09:54:06 +0900

    fix: 불필요한 member요청이 계속 가는 버그 해결

--- FE/src/routes/rootLoader.ts
+++ FE/src/routes/rootLoader.ts
@@ -4,16 +4,17 @@ import { getMemberInfo } from '@/apis/member';
 import axios from 'axios';
 
 const rootLoader = async ({ queryClient }: { queryClient: QueryClient }) => {
-  await queryClient
-    .ensureQueryData({
-      queryKey: QUERY_KEY.MEMBER,
-      queryFn: getMemberInfo,
-    })
-    .catch((e) => {
-      if (axios.isAxiosError(e)) {
-        process.env.NODE_ENV === 'development' && console.error(e.toJSON());
-      }
-    });
+  !queryClient.getQueryState(QUERY_KEY.MEMBER) &&
+    (await queryClient
+      .prefetchQuery({
+        queryKey: QUERY_KEY.MEMBER,
+        queryFn: getMemberInfo,
+      })
+      .catch((e) => {
+        if (axios.isAxiosError(e)) {
+          process.env.NODE_ENV === 'development' && console.error(e.toJSON());
+        }
+      }));
 
   return true;
 };
