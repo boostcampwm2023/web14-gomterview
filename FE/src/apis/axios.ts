810b1ae1db6b108e6bba218aa769a74e815b33ce
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2023-12-09 15:01:01 +0900

    fix: 410 무한루프에 대한 break 지점 추가

--- FE/src/apis/axios.ts
+++ FE/src/apis/axios.ts
@@ -1,4 +1,6 @@
 import { API, BASE_URL } from '@/constants/api';
+import { QUERY_KEY } from '@constants/queryKey';
+import { useQueryClient } from '@tanstack/react-query';
 import axios, { AxiosError } from 'axios';
 
 const api = (() => {
@@ -16,13 +18,22 @@ const api = (() => {
   return axiosInstance;
 })();
 
+let error410Count = 0; // 410 에러 카운터
+
 api.interceptors.response.use(
   (response) => {
+    error410Count = 0;
     return response;
   },
   async (error: AxiosError) => {
+    const queryClient = useQueryClient();
     if (error.response?.status === 410) {
       try {
+        error410Count++;
+        if (error410Count >= 10) {
+          // EDGE: 비정상적인 410 반환 시 10번 이상 재시도 시 그냥
+          return Promise.reject(error);
+        }
         await api({
           method: 'patch',
           url: API.REISSUE,
@@ -30,6 +41,7 @@ api.interceptors.response.use(
 
         return api.request(error.config!);
       } catch (err) {
+        queryClient.setQueryData(QUERY_KEY.MEMBER, null);
         return Promise.reject(err);
       }
     }
