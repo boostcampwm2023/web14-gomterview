1dc182c40e8436e4b88cf1c2ebe0ba50a7a68794
Sumin <robolindasoo@gmail.com>
2023-12-10 14:30:55 +0900

    refactor: breakpoints 단계 추가하고 QuestionSelectionBox 반응형 대응

--- FE/src/styles/_breakpoints.ts
+++ FE/src/styles/_breakpoints.ts
@@ -7,6 +7,7 @@ export const breakpoints = {
   mobileS: '320px', //아이폰 미니 등 작은 디바이스
   mobileM: '375px', //일반적인 스마트폰 사이즈
   mobileL: '425px', //큰 스마트폰 사이즈
+  mobile: '576px', //부트스트랩 기준 모바일 사이즈
   tablet: '768px', //태블릿 세로, 모바일 가로
   laptop: '1024px', //테블릿 가로, 노트북
   laptopL: '1440px', //데스크탑
