d61df5881a7decf9d62850f787b68b0f96678522
JangAJang <janghee5395@gmail.com>
2023-12-11 18:10:01 +0900

    refactor: test.util.ts에서 단일 트랜잭션 컨텍스트에서 엔티티들이 데이터베이스에 등록되지 않는 문제 해결

--- BE/src/util/test.util.ts
+++ BE/src/util/test.util.ts
@@ -1,6 +1,6 @@
 import { Test } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DataSource, DataSourceOptions, EntitySchema } from 'typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
 import {
   DynamicModule,
   INestApplication,
@@ -9,14 +9,21 @@ import {
 import * as cookieParser from 'cookie-parser';
 import {
   addTransactionalDataSource,
+  getDataSourceByName,
   initializeTransactionalContext,
 } from 'typeorm-transactional';
+import { Member } from '../member/entity/member';
+import { Category } from '../category/entity/category';
+import { Workbook } from '../workbook/entity/workbook';
+import { Question } from '../question/entity/question';
+import { Answer } from '../answer/entity/answer';
+import { Video } from '../video/entity/video';
 
 export const createIntegrationTestModule = async (
   modules: unknown[],
   entities: unknown[],
 ) => {
-  const ormModule = await createTypeOrmModuleForTest(entities);
+  const ormModule = await createTypeOrmModuleForTest();
   modules.push(ormModule);
 
   return await Test.createTestingModule({
@@ -29,28 +36,27 @@ export const addAppModules = (app: INestApplication) => {
   app.useGlobalPipes(new ValidationPipe());
 };
 
-export const createTypeOrmModuleForTest = async (entities: unknown[]) => {
+export const createTypeOrmModuleForTest = async () => {
   const module = TypeOrmModule.forRootAsync({
     useFactory() {
-      return ormModuleForTest(entities);
+      return ormModuleForTest();
     },
     async dataSourceFactory(options) {
-      if (!options) {
-        throw new Error('Invalid options passed');
-      }
-
-      return addTransactionalDataSource(new DataSource(options));
+      return (
+        getDataSourceByName('default') ||
+        addTransactionalDataSource(new DataSource(options))
+      );
     },
   });
   initializeTransactionalContext();
   return module;
 };
 
-export const ormModuleForTest = (entities: unknown[]): DataSourceOptions => {
+export const ormModuleForTest = (): DataSourceOptions => {
   return {
     type: 'sqlite', // 또는 다른 테스트용 데이터베이스 설정
     database: ':memory:',
-    entities: entities as (string | EntitySchema<any>)[],
+    entities: [Member, Category, Workbook, Question, Answer, Video],
     synchronize: true,
   };
 };
