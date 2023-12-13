4c263878bd3e12baedf57662d626906e3107b119
JangAJang <janghee5395@gmail.com>
2023-12-13 19:02:19 +0900

    feat: 미들웨어에서 캐시 제어와 요청 url에 대한 로깅을 처리하는 기능 구현

--- BE/src/main.ts
+++ BE/src/main.ts
@@ -5,16 +5,25 @@ import { setupSwagger } from './config/swagger.config';
 import { CORS_CONFIG } from './config/cors.config';
 import * as cookieParser from 'cookie-parser';
 import { initializeTransactionalContext } from 'typeorm-transactional';
+import { LoggerService } from './config/logger.config';
 
 async function bootstrap() {
   initializeTransactionalContext();
   const app = await NestFactory.create(AppModule, {
     abortOnError: true,
   });
+  const expressApp = app.getHttpAdapter().getInstance();
+  const logger = new LoggerService('traffic');
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors(CORS_CONFIG);
   setupSwagger(app);
+  // 캐시 제어 미들웨어 등록
+  expressApp.use((req, res, next) => {
+    res.setHeader('Cache-Control', 'no-cache');
+    logger.info(req.url);
+    next();
+  });
   await app.listen(8080, '0.0.0.0');
 }
 
